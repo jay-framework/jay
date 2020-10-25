9bdbb3f153dc097544d1957f4d49eaa80fc1cada
yoavaa <yoav@wix.com>
2020-10-25 14:15:53 +0200

    prevent removing nodes of another group

--- src/shared/kindergarden.ts
+++ src/shared/kindergarden.ts
@@ -17,6 +17,8 @@ export class KindergartenGroup {
     }
 
     removeNode(node: HTMLElement) {
+        if (!this.children.has(node))
+            throw new Error('cannot remove a node that was not added by this group')
         this.kindergarten.parentNode.removeChild(node);
         this.children.delete(node);
     }
--- test/shared/kindergarden.test.ts
+++ test/shared/kindergarden.test.ts
@@ -54,7 +54,7 @@ describe('Kindergarten', () => {
         });
     });
 
-    describe('Kindergarten two conditional groups', () => {
+    describe('Kindergarten preserves the ordering between groups', () => {
         test('add nodes group1, group 2', () => {
 
             let {document, parent} = makeParent();
@@ -69,6 +69,24 @@ describe('Kindergarten', () => {
             expect(parent.childNodes[0]).toEqual(node1);
             expect(parent.childNodes[1]).toEqual(node2);
         });
+
+        test('do not allow removing nodes of another group', () => {
+
+            let {document, parent} = makeParent();
+            let kindergarden = new Kindergarten(parent);
+            let group1 = kindergarden.newGroup();
+            let group2 = kindergarden.newGroup();
+            let node1 = makeNode(document, 'text1');
+            let node2 = makeNode(document, 'text2');
+            group1.ensureNode(node1);
+            group2.ensureNode(node2);
+
+            expect(() => {
+            group1.removeNode(node2);
+            }).toThrow();
+
+        });
+
         test('add nodes group2, group 1', () => {
 
             let {document, parent} = makeParent();
@@ -82,6 +100,26 @@ describe('Kindergarten', () => {
 
             expect(parent.childNodes[0]).toEqual(node1);
             expect(parent.childNodes[1]).toEqual(node2);
+        });
+        test('group 1, group 2, group 3, add nodes to groups 1, 2, 3, then remove node 2, node 1, then add node 1', () => {
+
+            let {document, parent} = makeParent();
+            let kindergarden = new Kindergarten(parent);
+            let group1 = kindergarden.newGroup();
+            let group2 = kindergarden.newGroup();
+            let group3 = kindergarden.newGroup();
+            let node1 = makeNode(document, 'text1');
+            let node2 = makeNode(document, 'text2');
+            let node3 = makeNode(document, 'text3');
+            group1.ensureNode(node1);
+            group2.ensureNode(node2);
+            group3.ensureNode(node3);
+            group2.removeNode(node2);
+            group1.removeNode(node1);
+            group1.ensureNode(node1);
+
+            expect(parent.childNodes[0]).toEqual(node1);
+            expect(parent.childNodes[1]).toEqual(node3);
         });
 
     });
