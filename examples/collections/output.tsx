5e7660e2440961d629b35cc745c2daa3f75c8c2a
yoavaa <yoav@wix.com>
2020-10-26 17:31:51 +0200

    implement initial state for collection rendering

--- examples/collections/output.tsx
+++ examples/collections/output.tsx
@@ -1,4 +1,5 @@
 import {Kindergarten} from '../kindergarden.js'
+import {ITEM_ADDED, ITEM_REMOVED, listCompare} from "../list-compare.js";
 
 interface Item {
     name: string,
@@ -13,7 +14,7 @@ interface ViewState {
 }
 
 export default function render(viewState: ViewState) {
-    let lastViewState = viewState;
+    let lastViewState: ViewState = {items: [], title: ''};
     let root = document.createElement('div');
     let h1 = document.createElement('h1');
     root.appendChild(h1);
@@ -27,8 +28,36 @@ export default function render(viewState: ViewState) {
             h1.textContent = title;
     };
 
+    const createDiv = (item: Item) => {
+        let coolRoot = document.createElement('div');
+        let span1 = document.createElement('span');
+        span1.style.cssText = 'color:green';
+        span1.textContent = item.name;
+        let span2 = document.createElement('span');
+        span2.style.cssText = 'color:red';
+        span2.textContent = item.completed?'yes':'no';
+        let span3 = document.createElement('span');
+        span3.style.cssText = 'color:blue';
+        span3.textContent = item.cost.toString();
+        coolRoot.appendChild(span1);
+        coolRoot.appendChild(span2);
+        coolRoot.appendChild(span3);
+        return coolRoot;
+    }
+
     const reconsileCollection = (items) => {
-        
+        let instructions = listCompare(lastViewState.items, items, 'id');
+        instructions.forEach(instruction => {
+            if (instruction.action === ITEM_ADDED) {
+                group1.ensureNode(createDiv(instruction.item), instruction.pos)
+            }
+            else if (instruction.action === ITEM_REMOVED) {
+
+            }
+            else {
+                // item moved
+            }
+        })
     };
 
     const rerender = (newViewState) => {
