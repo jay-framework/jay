859f7be8062d5d8a2c07c1ccf4c916558218ee2b
yoavaa <yoav@wix.com>
2020-12-28 12:04:27 +0200

    updated for element2 changed interface

--- examples/basic/output2.tsx
+++ examples/basic/output2.tsx
@@ -1,14 +1,15 @@
-import {element as e} from '../element2';
+import {element as e} from '../../lib/element2';
 
 interface ViewState {
     text: string
 }
 
 export default function render(viewState: ViewState) {
-    return e('div', {}, [viewState.text], viewState,
-        (elem, newViewState, oldViewState) => {
-            if (oldViewState.text !== newViewState.text)
+    return e('div', {}, [viewState.text], viewState, viewState.text,
+        (elem, newViewState, state) => {
+            if (state !== newViewState.text)
                 elem.textContent = newViewState.text;
+            return newViewState.text
         });
 }
 
--- examples/composite/output2.tsx
+++ examples/composite/output2.tsx
@@ -1,4 +1,4 @@
-import {element as e} from '../element2';
+import {element as e} from '../../lib/element2';
 
 interface ViewState {
     text: string
@@ -7,16 +7,18 @@ interface ViewState {
 
 export default function render(viewState: ViewState) {
     return e('div', {}, [
-        e('div', {}, [viewState.text], viewState,
-            (elem, newViewState, oldViewState) => {
-                if (oldViewState.text !== newViewState.text)
+        e('div', {}, [viewState.text], viewState, viewState.text,
+            (elem, newViewState, state) => {
+                if (state !== newViewState.text)
                     elem.textContent = newViewState.text;
+                return state;
             }),
         e('div', {}, ['static']),
-        e('div', {}, [viewState.text2], viewState,
-            (elem, newViewState, oldViewState) => {
-                if (oldViewState.text2 !== newViewState.text2)
+        e('div', {}, [viewState.text2], viewState, viewState.text2,
+            (elem, newViewState, state) => {
+                if (state !== newViewState.text2)
                     elem.textContent = newViewState.text2;
+                return state;
             })
 
     ]);
