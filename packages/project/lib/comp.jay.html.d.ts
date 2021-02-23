7c4412526c4e64b1b957b7ae84dcec448deec33b
yoavaa <yoav@wix.com>
2021-02-23 13:40:22 +0200

    example project

---
+++ packages/project/lib/comp.jay.html.d.ts
@@ -0,0 +1,21 @@
+import {JayElement} from "../runtime/lib/element";
+
+interface O1 {
+    s2: string,
+    n2: number
+}
+
+interface A1 {
+    s3: string,
+    n3: number
+}
+
+interface ViewState {
+    s1: string,
+    n1: number,
+    b1: boolean,
+    o1: O1,
+    a1: Array<A1>
+}
+
+export declare function render(viewState: ViewState): JayElement<ViewState>;
