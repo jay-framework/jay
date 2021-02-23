c271c2c6d0c3300550e49a078826f14d89791205
yoavaa <yoav@wix.com>
2021-02-23 16:12:29 +0200

    organizing the test project

---
+++ packages/project/lib/index.ts
@@ -0,0 +1,20 @@
+import {render} from './comp.jay.html';
+
+let initialData = {
+    s1: 'some string',
+    n1: 12,
+    b1: false,
+    o1: {
+        s2: 'sss',
+        n2: 23
+    },
+    a1: []
+}
+
+window.onload = function() {
+    let target = document.getElementById('target');
+
+    let {dom, update} = render(initialData);
+    target.innerHTML = '';
+    target.appendChild(dom);
+}
