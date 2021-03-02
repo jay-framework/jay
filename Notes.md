da7f1b870cf4770c117bf6eee41a05a30518375c
yoavaa <yoav@wix.com>
2021-03-02 16:01:15 +0200

    added structure to compiler to cope with optional imports

---
+++ Notes.md
@@ -0,0 +1,23 @@
+Runtime
+---
+
+- [x] basics - elements, strings
+- [x] update function
+- [ ] type formatters  
+- [ ] inputs
+- [ ] events
+- [ ] slots
+- [ ] composites
+- [ ] server side rendering
+- [ ] hydration
+
+Compiler
+---
+
+- [x] Typescript definition file
+- [ ] Basics - elements, strings
+
+Testing
+---
+
+- [ ] generate component drivers
--- packages/compiler/lib/compiler.ts
+++ packages/compiler/lib/compiler.ts
@@ -35,44 +35,107 @@ export function generateTypes(types: JayType): string {
     return renderInterface(types, 'ViewState');
 }
 
-function renderImports(jayElement: boolean, e: boolean, dt: boolean): string {
-    let imports = [];
-    if (jayElement) imports.push('JayElement');
-    if (e) imports.push('element as e');
-    if (dt) imports.push('dynamicText as dt');
-    return `import {${imports.join(', ')}} from "jay-runtime";`;
+
+enum Import {
+    jayElement,
+    element,
+    dynamicText
+}
+class Imports {
+    private readonly imports: Array<boolean>;
+    constructor(newImports: Array<boolean>) {
+        this.imports = newImports;
+    }
+
+    plus(addImport: Import) {
+        let newImports: Array<boolean> = [...this.imports];
+        newImports[addImport] = true;
+        return new Imports(newImports)
+    }
+
+    has(anImport: Import) {
+        return !!this.imports[anImport];
+    }
+    static none(): Imports {
+        return new Imports([]);
+    }
+    static for(...imports: Array<Import>): Imports {
+        let newImports = Imports.none();
+        imports.forEach(anImport => newImports = newImports.plus(anImport))
+        return newImports;
+    }
+    static merge(imports1: Imports, imports2: Imports) {
+        let merged = [];
+        for (let i =0; i < Math.max(imports1.imports.length, imports2.imports.length); i++)
+            merged[i] = imports1.imports[i] || imports2.imports[i];
+        return new Imports(merged);
+    }
+}
+
+function renderImports(imports: Imports): string {
+    let renderedImports = [];
+    if (imports.has(Import.jayElement)) renderedImports.push('JayElement');
+    if (imports.has(Import.element)) renderedImports.push('element as e');
+    if (imports.has(Import.dynamicText)) renderedImports.push('dynamicText as dt');
+    return `import {${renderedImports.join(', ')}} from "jay-runtime";`;
 }
 
 function renderFunctionDecleration(): string {
     return `export declare function render(viewState: ViewState): JayElement<ViewState>`;
 }
 
+class RenderFragment {
+    rendered: string;
+    imports: Imports;
+    constructor(rendered: string, imports: Imports) {
+        this.rendered = rendered;
+        this.imports = imports;
+    }
+
+    static empty(): RenderFragment {
+        return new RenderFragment('', Imports.none())
+    }
+
+    static merge(fragment1: RenderFragment, fragment2: RenderFragment): RenderFragment {
+        if (!!fragment1.rendered && !!fragment2.rendered)
+            return new RenderFragment(`${fragment1.rendered}, ${fragment2.rendered}`,
+                Imports.merge(fragment1.imports, fragment2.imports))
+        else if (!!fragment1.rendered)
+            return fragment1
+        else
+            return fragment2;
+    }
+}
+
 const parseText = /{(.+?)}/g
-function renderTextNode(currentDataVar: string, text: string): string {
+function renderTextNode(currentDataVar: string, text: string): RenderFragment {
     let isTemplateString = false;
     let templateString = text.replace(parseText, (fullMatch,group1) => {
         isTemplateString = true;
-        // todo validate against type
+        // todo handle different types and type formatters
         return `\${vs.${group1}}`;
     })
 
     if (!isTemplateString)
-        return `'${text}'`
+        return new RenderFragment(`'${text}'`, Imports.none())
     else {
         // todo add import dt
-        return `dt(${currentDataVar}, vs => \`${templateString}\`)`;
+        return new RenderFragment(`dt(${currentDataVar}, vs => \`${templateString}\`)`, Imports.for(Import.dynamicText));
     }
 
 }
 
-function renderNode(currentDataVar: string, node: Node): string {
+function renderNode(currentDataVar: string, node: Node): RenderFragment {
     switch(node.nodeType) {
         case NodeType.TEXT_NODE:
             let text = node.innerText;
             return renderTextNode(currentDataVar, text);
         case NodeType.ELEMENT_NODE:
             let htmlElement = node as HTMLElement;
-            return `e('${htmlElement.rawTagName}', {}, [${node.childNodes.map(_ => renderNode(currentDataVar, _)).join(', ')}])`
+            let childRenders = node.childNodes.map(_ => renderNode(currentDataVar, _))
+                .reduce((prev, current) => RenderFragment.merge(prev, current), RenderFragment.empty())
+            return new RenderFragment(`e('${htmlElement.rawTagName}', {}, [${childRenders.rendered}])`,
+                childRenders.imports.plus(Import.element));
         case NodeType.COMMENT_NODE:
             break
     }
@@ -83,18 +146,19 @@ function firstElementChild(node: Node): HTMLElement {
     return node.childNodes.find(child => child.nodeType === NodeType.ELEMENT_NODE) as HTMLElement;
 }
 
-function renderFunctionImplementation(rootBodyElement: HTMLElement): string {
-    let body = `export function render(viewState: ViewState): JayElement<ViewState> {\n`;
-    body += '  return ' + renderNode(`viewState`, firstElementChild(rootBodyElement)) + '\n}';
-
-    return body;
+function renderFunctionImplementation(rootBodyElement: HTMLElement): RenderFragment {
+    let renderedRoot = renderNode(`viewState`, firstElementChild(rootBodyElement));
+    let body = `export function render(viewState: ViewState): JayElement<ViewState> {
+  return ${renderedRoot.rendered}
+}`;
+    return new RenderFragment(body, renderedRoot.imports);
 }
 
 export function generateDefinitionFile(html): WithValidations<string> {
     let parsedFile = parseJayFile(html);
     return parsedFile.map((jayFile: JayFile) => {
         let types = generateTypes(jayFile.types);
-        return [renderImports(true, false, false),
+        return [renderImports(Imports.for(Import.jayElement)),
             types,
             renderFunctionDecleration()
         ].join('\n\n');
@@ -105,9 +169,10 @@ export function generateRuntimeFile(html): WithValidations<string> {
     let parsedFile = parseJayFile(html);
     return parsedFile.map((jayFile: JayFile) => {
         let types = generateTypes(jayFile.types);
-        return [renderImports(false, true, false),
+        let renderedImplementation = renderFunctionImplementation(jayFile.body);
+        return [renderImports(renderedImplementation.imports.plus(Import.element)),
             types,
-            renderFunctionImplementation(jayFile.body)
+            renderedImplementation.rendered
         ].join('\n\n');
     })
 }
