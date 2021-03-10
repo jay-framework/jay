c7918ff0dba10acc7ea218be0152be93379ec398
yoavaa <yoav@wix.com>
2021-03-10 10:18:06 +0200

    added the definition fixture

---
+++ packages/compiler/test/fixtures/collections/generated.d.ts
@@ -0,0 +1,16 @@
+import {JayElement} from "jay-runtime";
+
+interface Item {
+  name: string,
+  completed: boolean,
+  cost: number,
+  id: string
+}
+
+interface ViewState {
+  title: string,
+  items: Array<Item>
+}
+
+export declare function render(viewState: ViewState): JayElement<ViewState>
+
