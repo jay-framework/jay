ff89f80204302b6f1fb80862e15d7d2f18095999
yoavaa <yoav@wix.com>
2021-03-11 09:27:34 +0200

    add collection to the example project

--- packages/project/lib/index.ts
+++ packages/project/lib/index.ts
@@ -8,7 +8,7 @@ let initialData = {
         s2: 'sss',
         n2: 23
     },
-    a1: []
+    a1: [{s3: 'abc', n3: 123}, {s3: 'def', n3: 456}]
 }
 
 window.onload = function() {
@@ -22,6 +22,7 @@ window.onload = function() {
     let interval = setInterval(() => {
         initialData.n1 = count++;
         initialData.b1 = count % 2 === 0;
+        initialData.a1.push({s3: "id" + count, n3: count})
         update(initialData);
         if (count == 30)
             clearInterval(interval)
