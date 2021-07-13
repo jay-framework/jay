b33e8c5176eb0103f8203ceba3681640966e8db2
yoavaa <yoav@wix.com>
2021-07-13 14:22:48 +0300

    added tests for references and events

--- packages/runtime/test/lib/conditional-elements.test.ts
+++ packages/runtime/test/lib/conditional-elements.test.ts
@@ -63,5 +63,41 @@ describe('conditional-element', () => {
         })
 
     })
+
+    describe('references and events', () => {
+        interface ConditionalElement extends JayElement<ViewState> {
+            text1: HTMLElement,
+            text2: HTMLElement
+        }
+
+        function makeElement(data: ViewState): ConditionalElement {
+
+            return ConstructContext.withRootContext(data, (context: ConstructContext<[ViewState]>) =>
+                // noinspection DuplicatedCode
+                de('div', {}, [
+                    conditional((newViewState) => newViewState.condition,
+                        e('div', {style: {cssText: 'color:red'}, "ref":"text1"}, [dt(context, data => data.text1)], context)
+                    ),
+                    conditional((newViewState) => !newViewState.condition,
+                        e('div', {style: {cssText: 'color:green'}, "ref":"text2"}, [dt(context, data => data.text2)], context)
+                    )
+                ], context)) as ConditionalElement
+        }
+
+        it('should have references to elements under conditional', () => {
+            let jayElement = makeElement({text1: SOME_VALUE, condition: true, text2: ANOTHER_VALUE});
+            expect(jayElement.text1).toBeDefined()
+            expect(jayElement.text2).toBeDefined()
+        })
+
+        it('should register and invoke events', () => {
+            let jayElement = makeElement({text1: SOME_VALUE, condition: true, text2: ANOTHER_VALUE});
+            let mockCallback = jest.fn(_ => undefined);
+            jayElement.text1.onclick = mockCallback;
+            jayElement.text1.click();
+            expect(mockCallback.mock.calls.length).toBe(1);
+        })
+
+    });
 });
 
