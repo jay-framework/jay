bb0b2fc0c3314d3f5f85a89e4f6a0095ddf4bb77
yoavaa <yoav@wix.com>
2022-01-08 19:58:06 +0200

    react benchmark with basic and collection cases

---
+++ examples-other-frameworks/mini-benchmark-react/src/App.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import './App.css';
+import { Basic } from './basic';
+import {Collections} from "./collections";
+
+export enum SelectedExample {
+    basic,
+    collections,
+    composite,
+    conditions,
+    table
+}
+
+const examples = Object.keys(SelectedExample)
+    .filter(_ => !isNaN(Number(_)))
+    .map(_ => ({value: _, name: SelectedExample[_]}))
+
+function App() {
+
+    let [cycles, setCycles] = useState(1000)
+    let [selectedExample, setSelectedExample] = useState(SelectedExample.basic)
+    let [progress, setProgress] = useState('')
+    let [running, setRunning] = useState(false);
+
+    const cyclesChanged = (event) =>
+        setCycles(event.target.value)
+
+    const chooseExample = (event) => {
+        let index = event.target.selectedIndex;
+        setSelectedExample(Number(examples[index].value));
+    }
+
+    const progressCallback = (message: string, done: boolean) => {
+        setProgress(message);
+        if (done)
+            setRunning(false);
+    }
+
+    const run = () => setRunning(true);
+
+    return (
+        <div className="App">
+            <div className="title">Jay Examples</div>
+            <div className="select-example">
+                <label htmlFor="choose-example">Select example to view</label>
+                <select id="choose-example" onChange={chooseExample}>
+                    {examples.map(example =>
+                        (<option key={example.value} value={example.value}>{example.name}</option>)
+                    )}
+                </select>
+            </div>
+            <div className="cycles"><label htmlFor="cycles">Select number of cycles</label>
+                <input id="cycles" value={cycles} onInput={cyclesChanged}/>
+            </div>
+            <div className="progress">{progress}</div>
+            <button onClick={run}>run</button>
+            <div className="stage">
+                {(selectedExample === SelectedExample.basic) &&
+                (<Basic cycles={cycles} progressCallback={progressCallback} running={running}/>)}
+
+                {(selectedExample === SelectedExample.collections) &&
+                (<Collections cycles={cycles} progressCallback={progressCallback} running={running}/>)}
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ examples-other-frameworks/mini-benchmark-react/src/basic.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import benchmark from "./benchmark";
+
+
+interface BasicProps {
+    cycles: number,
+    running: boolean,
+    progressCallback: (string, boolean) => void,
+}
+
+export function Basic({cycles, running, progressCallback}: BasicProps) {
+    let [text, setText] = useState('name')
+
+    useEffect(() => {
+        if (running) {
+            benchmark(index => {
+                setText('name ' + index)
+            }, cycles, progressCallback);
+        }
+        }, [running]
+    )
+
+    return (
+        <div>
+            <div>{text}</div>
+        </div>)
+}
