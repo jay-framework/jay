aaf63f17bb31a27d288b77e9a12f744eaea2b97f
yoav <yoavaa@gmail.com>
2023-04-04 19:11:56 +0300

    tests for nested for each structures

--- packages/secure/test/sandbox-refs.test.ts
+++ packages/secure/test/sandbox-refs.test.ts
@@ -110,34 +110,39 @@ describe('sandbox-refs', () => {
         })
     });
 
-    describe('dynamic forEach refs', () => {
-        const vs = {items: [
+    describe('dynamic forEach refs - one level', () => {
+        const baseViewState = {items: [
                 {name: 'A', title: 'Alpha'},
                 {name: 'B', title: 'Beta'},
                 {name: 'C', title: 'Gamma'}
             ]}
-        const vs2 = {items: [
+        const addItemViewState = {items: [
                 {name: 'A', title: 'Alpha'},
                 {name: 'B', title: 'Beta'},
                 {name: 'C', title: 'Gamma'},
                 {name: 'D', title: 'Delta'}
             ]}
-        const vs3 = {items: [
+        const addAndRemoveItemViewState = {items: [
                 {name: 'A', title: 'Alpha'},
                 {name: 'C', title: 'Gamma'},
                 {name: 'D', title: 'Delta'}
             ]}
-        const vs4 = {items: [
+        const updateItemViewState = {items: [
                 {name: 'A', title: 'Alpha'},
                 {name: 'B', title: 'Beta Beta'},
                 {name: 'C', title: 'Gamma'}
             ]}
 
-        it('should register events --> JPMAddEventListener', () => {
+        function setup() {
             let endpoint = mkEndpoint();
-            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
+            let bridgeElement = mkBridgeElement(baseViewState, endpoint,() => [
                 forEach(vs => vs.items, 'name', () => [de('one')])
             ], ['one'])
+            return {endpoint, bridgeElement}
+        }
+
+        it('should register events --> JPMAddEventListener', () => {
+            let {endpoint, bridgeElement} = setup();
 
             bridgeElement.refs.one.onclick(() => {});
 
@@ -148,24 +153,18 @@ describe('sandbox-refs', () => {
         })
 
         it('should trigger events on JPMDomEvent --> callback', () => {
-            let endpoint = mkEndpoint();
-            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
-                forEach(vs => vs.items, 'name', () => [de('one')])
-            ], ['one'])
+            let {endpoint, bridgeElement} = setup();
             let callback = jest.fn();
 
             bridgeElement.refs.one.onclick(callback);
             endpoint.invoke(domEventMessage('click', ['B', 'one']))
 
             expect(callback.mock.calls).toHaveLength(1)
-            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["B","one"], "event": "click", "viewState": vs.items[1]})
+            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["B","one"], "event": "click", "viewState": baseViewState.items[1]})
         })
 
         it('in case of event with coordinate of non existing element, should not throw error, but instead return undefined viewState', () => {
-            let endpoint = mkEndpoint();
-            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
-                forEach(vs => vs.items, 'name', () => [de('one')])
-            ], ['one'])
+            let {endpoint, bridgeElement} = setup();
             let callback = jest.fn();
 
             bridgeElement.refs.one.onclick(callback);
@@ -176,13 +175,10 @@ describe('sandbox-refs', () => {
         })
 
         it('in case of event with coordinate of a removed element, should not throw error, but instead return undefined viewState', () => {
-            let endpoint = mkEndpoint();
-            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
-                forEach(vs => vs.items, 'name', () => [de('one')])
-            ], ['one'])
+            let {endpoint, bridgeElement} = setup();
             let callback = jest.fn();
 
-            bridgeElement.update(vs3);
+            bridgeElement.update(addAndRemoveItemViewState);
             bridgeElement.refs.one.onclick(callback);
             endpoint.invoke(domEventMessage('click', ['B', 'one']))
 
@@ -191,35 +187,88 @@ describe('sandbox-refs', () => {
         })
 
         it('should support viewState updates - additional item', () => {
-            let endpoint = mkEndpoint();
-            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
-                forEach(vs => vs.items, 'name', () => [de('one')])
-            ], ['one'])
+            let {endpoint, bridgeElement} = setup();
             let callback = jest.fn();
 
-            bridgeElement.update(vs2)
+            bridgeElement.update(addItemViewState)
             bridgeElement.refs.one.onclick(callback);
             endpoint.invoke(domEventMessage('click', ['D', 'one']))
 
             expect(callback.mock.calls).toHaveLength(1)
-            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["D","one"], "event": "click", "viewState": vs2.items[3]})
+            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["D","one"], "event": "click", "viewState": addItemViewState.items[3]})
         })
 
         it('should support viewState updates - updated item', () => {
-            let endpoint = mkEndpoint();
-            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
-                forEach(vs => vs.items, 'name', () => [de('one')])
-            ], ['one'])
+            let {endpoint, bridgeElement} = setup();
             let callback = jest.fn();
 
-            bridgeElement.update(vs4)
+            bridgeElement.update(updateItemViewState)
             bridgeElement.refs.one.onclick(callback);
             endpoint.invoke(domEventMessage('click', ['B', 'one']))
 
             expect(callback.mock.calls).toHaveLength(1)
-            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["B","one"], "event": "click", "viewState": vs4.items[1]})
+            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["B","one"], "event": "click", "viewState": updateItemViewState.items[1]})
         })
     });
+
+    describe('dynamic foreach refs - multi-level', () => {
+        const vs = {items: [
+                {name: 'A', title: 'Alpha', subItems: [{id: '1', name: 'one'}, {id: '2', name: 'two'}]},
+                {name: 'B', title: 'Beta', subItems: [{id: '3', name: 'three'}, {id: '4', name: 'four'}]},
+                {name: 'C', title: 'Gamma', subItems: [{id: '5', name: 'five'}, {id: '6', name: 'six'}]},
+                {name: 'D', title: 'Delta', subItems: [{id: '7', name: 'seven'}, {id: '8', name: 'eight'}]},
+            ]}
+        const vs2 = {items: [
+                {name: 'A', title: 'Alpha', subItems: [{id: '1', name: 'one'}, {id: '2', name: 'two'}, {id: '2.5', name: 'two and half'}]},
+                {name: 'B', title: 'Beta', subItems: [{id: '3', name: 'three'}]},
+                {name: 'C', title: 'Gamma', subItems: [{id: '5', name: 'five changed'}, {id: '6', name: 'six'}]},
+                {name: 'E', title: 'epsilon', subItems: [{id: '9', name: 'nine'}, {id: '10', name: 'ten'}]},
+            ]}
+
+        type VS = typeof vs;
+        type VSItem = typeof vs.items[number]
+        type VSSubItem = VSItem["subItems"][number]
+
+        function setup() {
+            let endpoint = mkEndpoint();
+            let bridgeElement = mkBridgeElement(vs, endpoint,() => [
+                forEach<VS, VSItem>(vs => vs.items, 'name', () => [
+                    de('one'),
+                    forEach<VSItem, VSSubItem>(vs => vs.subItems, 'id', () => [
+                        de('two')
+                    ])
+                ])
+            ], ['one', 'two'])
+            return {endpoint, bridgeElement}
+        }
+
+        it('should register events --> JPMAddEventListener', () => {
+            let {endpoint, bridgeElement} = setup();
+
+            bridgeElement.refs.one.onclick(() => {});
+            bridgeElement.refs.two.onclick(() => {});
+
+            expect(endpoint.outMessages).toHaveLength(2)
+            expect(endpoint.outMessages[0].type).toBe(JayPortMessageType.addEventListener)
+            expect(endpoint.outMessages[0].eventType).toBe('click')
+            expect(endpoint.outMessages[0].refName).toBe('one')
+            expect(endpoint.outMessages[1].type).toBe(JayPortMessageType.addEventListener)
+            expect(endpoint.outMessages[1].eventType).toBe('click')
+            expect(endpoint.outMessages[1].refName).toBe('two')
+        })
+
+        it('should trigger events on JPMDomEvent --> callback', () => {
+            let {endpoint, bridgeElement} = setup();
+            let callback = jest.fn();
+
+            bridgeElement.refs.two.onclick(callback);
+            endpoint.invoke(domEventMessage('click', ['B', '3', 'two']))
+
+            expect(callback.mock.calls).toHaveLength(1)
+            expect(callback.mock.calls[0][0]).toEqual({"coordinate": ["B", '3', "two"], "event": "click", "viewState": vs.items[1].subItems[0]})
+        })
+
+    })
 })
 
 interface TestJayEndpoint extends JayEndpoint {
