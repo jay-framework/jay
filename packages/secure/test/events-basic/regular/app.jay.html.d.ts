2adc4f56a031ecd51231cce1946b08e81cf2f074
yoav <yoavaa@gmail.com>
2023-04-19 15:22:40 +0300

    added .d.ts files from testing

---
+++ packages/secure/test/events-basic/regular/app.jay.html.d.ts
@@ -0,0 +1,12 @@
+import {JayElement, RenderElementOptions} from "jay-runtime";
+import {Counter} from './counter';
+
+export interface AppViewState {
+
+}
+
+export interface AppElementRefs {}
+
+export type AppElement = JayElement<AppViewState, AppElementRefs>
+
+export declare function render(viewState: AppViewState, options?: RenderElementOptions): AppElement
---
+++ packages/secure/test/events-basic/regular/counter-refs.d.ts
@@ -0,0 +1,15 @@
+import {JayComponent, EventEmitter, ComponentCollectionProxy, EventTypeFrom, PropsFrom, ViewStateFrom, ElementFrom} from 'jay-runtime';
+import {Counter} from "./counter";
+
+export type CounterComponentType = ReturnType<typeof Counter>;
+
+export interface CounterRef<ParentVS> extends JayComponent<
+  PropsFrom<CounterComponentType>,
+  ViewStateFrom<CounterComponentType>,
+  ElementFrom<CounterComponentType>>{
+  
+}
+
+export interface CounterRefs<ParentVS> extends ComponentCollectionProxy<ParentVS, CounterRef<ParentVS>> {
+  
+}
