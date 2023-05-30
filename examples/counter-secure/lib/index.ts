1347445a96ffb76a9b2caa2e0138dee9a66bd1cf
yoav <yoavaa@gmail.com>
2023-05-30 17:03:54 -0400

    new example for counter working as a secure component

---
+++ examples/counter-secure/lib/index.ts
@@ -0,0 +1,11 @@
+import {Counter} from './counter';
+
+window.onload = function() {
+    let target = document.getElementById('target');
+    let counter = Counter({initialValue: 12});
+    target.innerHTML = '';
+    target.appendChild(counter.element.dom);
+}
+
+
+
