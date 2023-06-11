30522e3c9e9299c886e9fb391a069f3bf2988170
yoav <yoavaa@gmail.com>
2023-06-11 12:26:20 +0300

    jay-reactive 1.0.4

--- packages/reactive/lib/index.ts
+++ packages/reactive/lib/index.ts
@@ -1,3 +1,5 @@
 export * from './reactive';
 export {getRevision, touchRevision, REVISION, Revisioned, checkModified} from './revisioned';
 export {mutableObject, isMutable, addMutableListener, removeMutableListener} from './mutable';
+export {deserialize} from './mutable-deserializer'
+export {serialize} from './mutable-serializer'
