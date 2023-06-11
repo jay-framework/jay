61f978e3954cb53e40cfa4e9acc6702313c4154f
yoav <yoavaa@gmail.com>
2023-06-11 14:24:04 +0300

    testing immutable serialization

--- packages/reactive/test/mutable-serialization.test.ts
+++ packages/reactive/test/mutable-serialization.test.ts
@@ -1,10 +1,10 @@
 import {describe, expect, it, jest} from '@jest/globals'
 import {getRevision, mutableObject} from "../lib";
-import {serialize} from "../lib/mutable-serializer";
-import {deserialize} from "../lib/mutable-deserializer";
+import {serialize} from "../lib";
+import {deserialize} from "../lib";
 
 describe("mutable serialization", () => {
-    describe("simple objects", () => {
+    describe("simple mutable objects", () => {
         const SIMPLE_OBJECT = {a: 1, b:2, c: "abcd", d: true};
 
         it("should deserialize to an equal object", () => {
@@ -41,7 +41,30 @@ describe("mutable serialization", () => {
         })
     })
 
-    describe("nested object", () => {
+    describe("simple immutable objects", () => {
+        const SIMPLE_OBJECT = {a: 1, b:2, c: "abcd", d: true};
+
+        it("should deserialize to an equal object", () => {
+            let immutable = SIMPLE_OBJECT;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+            expect(immutable).toEqual(deserialized);
+        })
+
+        it("should re-serialize and re-deserialize to an equal, yet different object", () => {
+            let immutable = SIMPLE_OBJECT;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+
+            immutable = {...immutable, a: 12, c: 'efgh', d: false};
+            [serialized, nextSerialize] = nextSerialize(immutable);
+            let [deserialized2, nextDeserialize2] = nextDeserialize(serialized)
+            expect(immutable).toEqual(deserialized2);
+            expect(deserialized).not.toBe(deserialized2);
+        })
+    })
+
+    describe("nested mutable object", () => {
         const NESTED_OBJECT = {
             a: 1, b:2, name: {firstName: "Joe", lastName: "Smith"}
         };
@@ -82,7 +105,34 @@ describe("mutable serialization", () => {
 
     })
 
-    describe("array of primitives", () => {
+    describe("nested immutable object", () => {
+        const NESTED_OBJECT = {
+            a: 1, b:2, name: {firstName: "Joe", lastName: "Smith"}
+        };
+        const NESTED_OBJECT_UPDATE = {firstName: "Mark", lastName: "Webber"}
+
+        it("should deserialize to an equal object", () => {
+            let immutable = NESTED_OBJECT;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+            expect(immutable).toEqual(deserialized);
+        })
+
+        it("should re-serialize and re-deserialize to an equal, yes different object", () => {
+            let immutable = NESTED_OBJECT;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+
+            immutable = {...immutable, name: NESTED_OBJECT_UPDATE};
+            [serialized, nextSerialize] = nextSerialize(immutable);
+            let [deserialized2, nextDeserialize2] = nextDeserialize(serialized)
+            expect(immutable).toEqual(deserialized2);
+            expect(deserialized).not.toBe(deserialized2);
+        })
+
+    })
+
+    describe("mutable array of primitives", () => {
         const ARRAY_PRIMITIVES = {
             items: [1,2,3,4]
         }
@@ -123,7 +173,34 @@ describe("mutable serialization", () => {
 
     })
 
-    describe("array of objects", () => {
+    describe("immutable array of primitives", () => {
+        const ARRAY_PRIMITIVES = {
+            items: [1,2,3,4]
+        }
+        const ARRAY_PRIMITIVES_UPDATE = [1,2,3,4,5]
+
+        it("should deserialize to an equal object", () => {
+            let immutable = ARRAY_PRIMITIVES;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+            expect(immutable).toEqual(deserialized);
+        })
+
+        it("should re-serialize and re-deserialize to an equal, yet different object", () => {
+            let immutable = ARRAY_PRIMITIVES;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+
+            immutable = {...immutable, items: ARRAY_PRIMITIVES_UPDATE};
+            [serialized, nextSerialize] = nextSerialize(immutable);
+            let [deserialized2, nextDeserialize2] = nextDeserialize(serialized)
+            expect(immutable).toEqual(deserialized2);
+            expect(deserialized).not.toBe(deserialized2);
+        })
+
+    })
+
+    describe("mutable array of objects", () => {
         const ARRAY_OBJECTS = {
             items: [
                 {id: "one", name: "Joe", age: 12},
@@ -170,6 +247,63 @@ describe("mutable serialization", () => {
                 let [deserialized2, nextDeserialize2] = nextDeserialize(serialized)
                 expect(mutable).toEqual(deserialized2);
                 expect(deserialized).toBe(deserialized2);
+            })
+
+            it.skip("move array items on re-deserialization", () => {
+                let mutable = mutableObject(ARRAY_OBJECTS);
+                let [serialized, nextSerialize] = serialize(mutable);
+                let [deserialized, nextDeserialize] = deserialize<any>(serialized);
+
+                // replace items 1 and 2
+                let i1 = mutable.items[1];
+                let i2 = mutable.items[2];
+                mutable.items[1] = i2;
+                mutable.items[2] = i1;
+
+                let deserializedItem_1 = deserialized.items[1];
+                let deserializedItem_2 = deserialized.items[2];
+                [serialized, nextSerialize] = nextSerialize(mutable);
+                let [deserialized2, nextDeserialize2] = nextDeserialize(serialized)
+                expect(mutable).toEqual(deserialized2);
+                expect(deserialized).toBe(deserialized2);
+                expect(deserialized.items[1]).toBe(deserializedItem_2)
+                expect(deserialized.items[2]).toBe(deserializedItem_1)
+            })
+        })
+
+    })
+
+    describe("immutable array of objects", () => {
+        const ARRAY_OBJECTS = {
+            items: [
+                {id: "one", name: "Joe", age: 12},
+                {id: "two", name: "Mark", age: 24},
+                {id: "three", name: "Bill", age: 53}
+            ]
+        }
+        const ARRAY_OBJECTS_ITEM_1_UPDATE = {id: "two", name: "Mark Smith", age: 37}
+
+        it("should deserialize to an equal object", () => {
+            let immutable = ARRAY_OBJECTS;
+            let [serialized, nextSerialize] = serialize(immutable);
+            let [deserialized, nextDeserialize] = deserialize(serialized);
+            expect(immutable).toEqual(deserialized);
+        })
+
+        describe('array mutations', () => {
+            it("array item update", () => {
+                let immutable = ARRAY_OBJECTS;
+                let [serialized, nextSerialize] = serialize(immutable);
+                let [deserialized, nextDeserialize] = deserialize(serialized);
+
+                immutable = {...immutable,
+                    items: immutable.items
+                        .map((item, index) => index === 1?ARRAY_OBJECTS_ITEM_1_UPDATE:item)
+                };
+                [serialized, nextSerialize] = nextSerialize(immutable);
+                let [deserialized2, nextDeserialize2] = nextDeserialize(serialized)
+                expect(immutable).toEqual(deserialized2);
+                expect(deserialized).not.toBe(deserialized2);
             })
 
             it.skip("move array items on re-deserialization", () => {
