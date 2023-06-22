c9a0d2c52cc9bbf1bca6f9ed68e6f3752d57ea8a
yoav <yoavaa@gmail.com>
2023-06-22 12:20:41 +0300

    reverted the optimization for mutable

--- packages/reactive/lib/mutable-serializer.ts
+++ packages/reactive/lib/mutable-serializer.ts
@@ -1,30 +1,14 @@
-import {isMutable, originalSymbol} from "./mutable";
-import {getRevision, REVISION} from "./revisioned";
-import {ARRAY, NOT_CHANGED, PROP_TO_REV, REVNUM} from "./serialize-consts";
-import {setPrivateProperty} from "./private-property";
+import {isMutable} from "./mutable";
+import {getRevision} from "./revisioned";
+import {ARRAY, REVNUM} from "./serialize-consts";
 
 export type Serialize = (mutable: any) => [string, Serialize]
 
 function replacer(key: string, value: any) {
     if (isMutable(value)) {
         let revisioned = getRevision(value)
-        let newValue = {}
+        let newValue = {...value}
         newValue[REVNUM] = revisioned.revNum
-        let propToRev = value[PROP_TO_REV];
-        let newPropToRev = {}
-        if (Array.isArray(value)) {
-
-        }
-        else {
-            for (let prop of Object.keys(value)) {
-                if (propToRev[prop] === value[prop][REVISION])
-                    newValue[prop] = NOT_CHANGED
-                else
-                    newValue[prop] = value[prop]
-                // newPropToRev[prop] =
-            }
-        }
-        // let newValue = {...value}
         if (Array.isArray(value)) {
             newValue[ARRAY] = true;
         }
@@ -34,54 +18,6 @@ function replacer(key: string, value: any) {
         return value;
 }
 
-function serializeEntity(entity: any): any {
-    let type = typeof entity;
-    if (type === 'object') {
-        if (Array.isArray(entity)) {
-            let newValue = {}
-            newValue[ARRAY] = true;
-            if (isMutable(entity)) {
-                let revisioned = getRevision(entity)
-                newValue[REVNUM] = revisioned.revNum
-            }
-            for (let prop of Object.keys(entity)) {
-                newValue[prop] = serializeEntity(entity[prop])
-            }
-            return JSON.stringify(newValue);
-        }
-        else {
-            let newValue = {}
-            if (isMutable(entity)) {
-                let newPropToRev = {}
-                let revisioned = getRevision(entity)
-                newValue[REVNUM] = revisioned.revNum
-                let propToRev = entity[PROP_TO_REV] || {};
-                for (let prop of Object.keys(entity)) {
-                    let entityPropValue = entity[prop];
-                    if (isMutable(entityPropValue)) {
-                        let propRevisioned = getRevision(entityPropValue);
-                        if (propToRev[prop] === propRevisioned.revNum)
-                            newValue[prop] = NOT_CHANGED
-                        else
-                            newValue[prop] = serializeEntity(entityPropValue)
-                        newPropToRev[prop] = propRevisioned.revNum;
-                    }
-                    else
-                        newValue[prop] = serializeEntity(entityPropValue)
-                }
-                setPrivateProperty(entity[originalSymbol], PROP_TO_REV, newPropToRev)
-            }
-            else {
-                for (let prop of Object.keys(entity)) {
-                    newValue[prop] = serializeEntity(entity[prop])
-                }
-            }
-            return newValue;
-        }
-    }
-    else return entity;
-}
-
 export function serialize(mutable: any): [string, Serialize] {
-    return [JSON.stringify(serializeEntity(mutable)), serialize]
+    return [JSON.stringify(mutable, replacer), serialize]
 }
