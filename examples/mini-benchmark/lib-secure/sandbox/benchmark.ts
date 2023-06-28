bc1f80ab13a7455ac8093599791d26ba7b22ce8b
yoav <yoavaa@gmail.com>
2023-06-28 09:50:21 +0300

    implemented requestAnimationFrame from main using exec$ in the benchmark example. it shows a problem that for some reason, rendering does not happen.

--- examples/mini-benchmark/lib-secure/sandbox/benchmark.ts
+++ examples/mini-benchmark/lib-secure/sandbox/benchmark.ts
@@ -1,4 +1,7 @@
 // benchmark
+import {exec$} from "jay-secure/dist/sandbox/exec";
+import {$funcGlobal} from "jay-secure/dist/$func";
+
 const defaultCycles = 1000;
 
 
@@ -7,13 +10,18 @@ export default function benchmark(action: (number) => void, cycles: number, prog
     cycles = cycles || defaultCycles;
     let i = 0;
     let frameStarts = [];
+
+    const requestAnimationFrame$ = async (callback) => {
+        await exec$($funcGlobal("3"))
+        callback()
+    }
     const animationFrame = () => {
         frameStarts.push(new Date().getTime());
         if (frameStarts.length > 50)
             frameStarts.shift();
         action(i++);
         if (i<cycles) {
-            requestAnimationFrame(animationFrame);
+            requestAnimationFrame$(animationFrame)
             if (i % 50 === 0) {
                 let end = new Date().getTime();
                 progressCallback(`frames: ${1000/((end-frameStarts[0])/(frameStarts.length+1))} cycles: ${i}`);
@@ -25,5 +33,5 @@ export default function benchmark(action: (number) => void, cycles: number, prog
         }
     };
 
-    requestAnimationFrame(animationFrame)
+    requestAnimationFrame$(animationFrame)
 }
