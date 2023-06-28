532be8a2777d57b5a2e2b9f69b370ad9b9e04443
yoav <yoavaa@gmail.com>
2023-06-28 17:27:27 +0300

    adding .d.ts files for tests

---
+++ packages/secure/test/exec-synthetic/regular/app.jay.html.d.ts
@@ -0,0 +1,12 @@
+import {JayElement, RenderElementOptions} from "jay-runtime";
+import {Comp} from './comp';
+
+export interface AppViewState {
+
+}
+
+export interface AppElementRefs {}
+
+export type AppElement = JayElement<AppViewState, AppElementRefs>
+
+export declare function render(viewState: AppViewState, options?: RenderElementOptions): AppElement
