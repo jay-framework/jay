db58b383befbba8f71125979fd8484a4e4048a1c
yoav <yoavaa@gmail.com>
2023-06-29 13:46:00 +0300

    cleaned reactive for breaking the package

---
+++ packages/reactive/lib/reactive-contract.ts
@@ -0,0 +1,13 @@
+export type ChangeListener = () => void;
+
+export interface MutableContract {
+    isMutable(): true
+    addMutableListener(changeListener: ChangeListener)
+    removeMutableListener(changeListener: ChangeListener)
+    getRevision(): number
+    setRevision(revNum: number)
+}
+
+export function isMutable(obj): obj is MutableContract {
+    return typeof obj === 'object' && typeof obj.isMutable === 'function' && obj.isMutable()
+}
--- packages/reactive/lib/revisioned.ts
+++ packages/reactive/lib/revisioned.ts
@@ -1,7 +1,7 @@
-import {setPrivateProperty} from "./private-property";
+// import {setPrivateProperty} from "./private-property";
+import {isMutable} from "./reactive-contract";
 
-
-export const REVISION = Symbol.for('revision');
+// export const REVISION = Symbol.for('revision');
 let nextRevision = 1;
 
 export interface Revisioned<T> {
@@ -10,24 +10,29 @@ export interface Revisioned<T> {
 }
 
 export function getRevision<T extends object>(value: T): Revisioned<T> {
-    return {value, revNum: value[REVISION] || NaN};
+    return {value, revNum: isMutable(value)?value.getRevision(): NaN};
 }
 
-export function setRevision<T extends object>(value: T, revision: number): T {
-    return setPrivateProperty(value, REVISION, revision);
+export function setRevision<T extends object>(value: T, revision: number) {
+    isMutable(value) && value.setRevision(revision);
 }
 
-export function initRevision<T extends object>(value: T): T {
-    return value[REVISION]?value:touchRevision(value)
+function getRevNum(value: any) {
+    return isMutable(value)?value.getRevision(): NaN;
 }
+
+// export function initRevision<T extends object>(value: T): T {
+//     return value[REVISION]?value:touchRevision(value)
+// }
 
 export function touchRevision<T extends object>(value: T): T {
-    return setRevision(value, nextRevision++);
+    setRevision(value, nextRevision++);
+    return value;
 }
 
 export function checkModified<T>(value: T, oldValue?: Revisioned<T>): [Revisioned<T>, boolean] {
     let isObject = typeof value === 'object';
-    let revNum = isObject? value[REVISION] || NaN : NaN;
+    let revNum = getRevNum(value);
     let newValue = {value, revNum};
     if (!oldValue)
         return [newValue, true]
