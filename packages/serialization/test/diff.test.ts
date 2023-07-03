ffcd8a95d2e2f6b4b59fa5819dff9d618d9ebea7
yoav <yoavaa@gmail.com>
2023-07-03 21:52:12 +0300

    more tests for objects

--- packages/serialization/test/diff.test.ts
+++ packages/serialization/test/diff.test.ts
@@ -85,6 +85,26 @@ describe('diff', () => {
             ])
         })
 
+        it("should return replace object with primitive", () => {
+            let patch = diff(
+                {a: 1, b: 2, c: 4},
+                {a: 1, b: 2, c: {d: 4, e: 5, f: 6}}
+            )
+            expect(patch[0]).toEqual([
+                {op: REPLACE, path: ['c'], value: 4}
+            ])
+        })
+
+        it("should return replace primitive with object", () => {
+            let patch = diff(
+                {a: 1, b: 2, c: {d: 4, e: 5, f: 6}},
+                {a: 1, b: 2, c: 4}
+            )
+            expect(patch[0]).toEqual([
+                {op: REPLACE, path: ['c'], value: {d: 4, e: 5, f: 6}}
+            ])
+        })
+
         it("should return replace an object if more then 50% of it's properties has been modified", () => {
             let patch = diff(
                 {a: 1, b: 2, c: {d: 7, e: 8, f: 9}},
@@ -92,6 +112,16 @@ describe('diff', () => {
             )
             expect(patch[0]).toEqual([
                 {op: REPLACE, path: ['c'], value: {d: 7, e: 8, f: 9}}
+            ])
+        })
+
+        it("should return replace an object if more then 50% of it's properties has been modified across multiple objects", () => {
+            let patch = diff(
+                {a: 1, b: 2, c: {d: 7, e: {x: 1, y: 4}, f: {x: 1, y: 4}}},
+                {a: 1, b: 2, c: {d: 4, e: {x: 1, y: 2}, f: {x: 1, y: 2}}}
+            )
+            expect(patch[0]).toEqual([
+                {op: REPLACE, path: ['c'], value: {d: 7, e: {x: 1, y: 4}, f: {x: 1, y: 4}}}
             ])
         })
     });
