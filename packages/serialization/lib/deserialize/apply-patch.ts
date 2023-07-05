b0fd38942300c3cb9c1db992035ed683c4d46492
yoav <yoavaa@gmail.com>
2023-07-05 23:21:22 +0300

    patch nested objects

--- packages/serialization/lib/deserialize/apply-patch.ts
+++ packages/serialization/lib/deserialize/apply-patch.ts
@@ -1,14 +1,15 @@
 import {ADD, JSONPatch, JSONPatchOperation, JSONPointer, REMOVE, REPLACE} from "../types";
 
 function applyPatchOperation(target: object, patchOperation: JSONPatchOperation) {
-    let dirLength = patchOperation.path.length - 1;
+    let {path, op} = patchOperation
+    let dirLength = path.length - 1;
     for (let i = 0; i < dirLength; i++) {
-        target = target[i];
+        target = target[path[i]];
     }
     if (patchOperation.op === REPLACE || patchOperation.op === ADD)
-        target[patchOperation.path[dirLength]] = patchOperation.value
-    else if (patchOperation.op === REMOVE)
-        delete target[patchOperation.path[dirLength]];
+        target[path[dirLength]] = patchOperation.value
+    else if (op === REMOVE)
+        delete target[path[dirLength]];
 }
 
 export function applyPatch(target: object, patch: JSONPatch) {
--- packages/serialization/test/apply-patch.test.ts
+++ packages/serialization/test/apply-patch.test.ts
@@ -4,30 +4,60 @@ import {applyPatch} from "../lib/deserialize/apply-patch";
 import {ADD, REMOVE, REPLACE} from "../lib/types";
 
 describe('apply JSON patch', () => {
-    it('should apply a replace patch on a flat object', () => {
-        let obj = mutableObject({a: 1, b: 2, c:3})
-        applyPatch(obj, [
-            {op: REPLACE, path: ['b'], value: 4},
-            {op: REPLACE, path: ['c'], value: 5}
-        ])
-        expect(obj).toEqual({a: 1, b: 4, c: 5});
+    describe("flat object", () => {
+        it('should apply a replace patch', () => {
+            let obj = mutableObject({a: 1, b: 2, c:3})
+            applyPatch(obj, [
+                {op: REPLACE, path: ['b'], value: 4},
+                {op: REPLACE, path: ['c'], value: 5}
+            ])
+            expect(obj).toEqual({a: 1, b: 4, c: 5});
+        })
+
+        it('should apply an add patch', () => {
+            let obj = mutableObject({a: 1, b: 2, c:3})
+            applyPatch(obj, [
+                {op: ADD, path: ['d'], value: 4}
+            ])
+            expect(obj).toEqual({a: 1, b: 2, c: 3, d: 4});
+        })
+
+        it('should apply a remove patch', () => {
+            let obj = mutableObject({a: 1, b: 2, c:3})
+            applyPatch(obj, [
+                {op: REMOVE, path: ['c']}
+            ])
+            expect(obj).toEqual({a: 1, b: 2});
+        })
     })
 
-    it('should apply an add patch on a flat object', () => {
-        let obj = mutableObject({a: 1, b: 2, c:3})
-        applyPatch(obj, [
-            {op: ADD, path: ['d'], value: 4}
-        ])
-        expect(obj).toEqual({a: 1, b: 2, c: 3, d: 4});
+    describe("nested object", () => {
+        it('should apply a replace patch', () => {
+            let obj = mutableObject({x: {a: 1, b: 2, c:3}})
+            applyPatch(obj, [
+                {op: REPLACE, path: ['x', 'b'], value: 4},
+                {op: REPLACE, path: ['x', 'c'], value: 5}
+            ])
+            expect(obj).toEqual({x: {a: 1, b: 4, c: 5}});
+        })
+
+        it('should apply an add patch', () => {
+            let obj = mutableObject({x: {a: 1, b: 2, c:3}})
+            applyPatch(obj, [
+                {op: ADD, path: ['x', 'd'], value: 4}
+            ])
+            expect(obj).toEqual({x: {a: 1, b: 2, c: 3, d: 4}});
+        })
+
+        it('should apply a remove patch', () => {
+            let obj = mutableObject({x: {a: 1, b: 2, c:3}})
+            applyPatch(obj, [
+                {op: REMOVE, path: ['x', 'c']}
+            ])
+            expect(obj).toEqual({x: {a: 1, b: 2}});
+        })
     })
 
-    it('should apply a remove patch on a flat object', () => {
-        let obj = mutableObject({a: 1, b: 2, c:3})
-        applyPatch(obj, [
-            {op: REMOVE, path: ['c']}
-        ])
-        expect(obj).toEqual({a: 1, b: 2});
-    })
 })
 
 
