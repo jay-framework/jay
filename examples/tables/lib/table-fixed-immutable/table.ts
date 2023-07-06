fd28cc7945fc95773fe37e8c51c9d95bd268f7ee
yoav <yoavaa@gmail.com>
2023-07-06 21:39:51 +0300

    implemented the immutable table

--- examples/tables/lib/table-fixed-immutable/table.ts
+++ examples/tables/lib/table-fixed-immutable/table.ts
@@ -1,6 +1,6 @@
 import {render, TableElementRefs} from './table.jay.html';
 import {createState, makeJayComponent, Props } from 'jay-component';
-import { mutableObject } from 'jay-mutable';
+import {Line} from "../table-immutable/table.jay.html";
 
 interface TableProps {
     tableSize: number
@@ -9,7 +9,7 @@ interface TableProps {
 
 function TableConstructor({tableSize, numCellsToUpdate}: Props<TableProps>, refs: TableElementRefs) {
 
-    let [line, _] = createState(() => {
+    let [line, setLine] = createState<Array<Line>>(() => {
         let tableLines = []
         for (let x = 0; x < tableSize(); x++) {
             tableLines[x] = {id: x, cell: []};
@@ -17,15 +17,20 @@ function TableConstructor({tableSize, numCellsToUpdate}: Props<TableProps>, refs
                 tableLines[x].cell[y] = {id: y, value: Math.round(Math.random()*100)};
             }
         }
-        return mutableObject(tableLines)
+        return tableLines
     });
 
     const updateData = (cycle: number) => {
+        let copy = line().map(aLine => ({
+            id: aLine.id,
+            cell: aLine.cell.map(cell => ({...cell}))
+        }))
         for (let i = 0; i < numCellsToUpdate(); i++) {
             let x = Math.floor(Math.random()*tableSize());
             let y = Math.floor(Math.random()*tableSize());
-            line()[x].cell[y].value = Math.round(Math.random()*100);
+            copy[x].cell[y].value = Math.round(Math.random()*100);
         }
+        setLine(copy);
     }
 
     return {
