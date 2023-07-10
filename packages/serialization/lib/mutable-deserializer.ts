1669df785fa81da0ffe50e2d984807589069986a
yoav <yoavaa@gmail.com>
2023-07-10 23:06:03 +0300

    variable renames in mutable-deserializer.ts

--- packages/serialization/lib/mutable-deserializer.ts
+++ packages/serialization/lib/mutable-deserializer.ts
@@ -3,23 +3,23 @@ import {ADD, JSONPatch, REPLACE} from "./types";
 import {patch} from "./deserialize/patch";
 import {MutableContract} from "jay-mutable";
 
-export type Deserialize<T> = (serialized: JSONPatch) => [T, Deserialize<T>]
-export function deserialize<T extends MutableContract>(serialized: JSONPatch): [T, Deserialize<T>] {
-    return _deserialize(undefined)(serialized)
+export type Deserialize<T> = (jsonPatch: JSONPatch) => [T, Deserialize<T>]
+export function deserialize<T extends MutableContract>(jsonPatch: JSONPatch): [T, Deserialize<T>] {
+    return _deserialize(undefined)(jsonPatch)
 }
 
-function _deserialize<T extends MutableContract>(mutable: T): (serialized: JSONPatch) => [T, Deserialize<T>] {
-    return (serialized: JSONPatch) => {
-        if (serialized.length === 1 && serialized[0].op === ADD && serialized[0].path.length === 0) {
-            mutable = mutableObject(serialized[0].value) as T;
+function _deserialize<T extends MutableContract>(mutable: T): (jsonPatch: JSONPatch) => [T, Deserialize<T>] {
+    return (jsonPatch: JSONPatch) => {
+        if (jsonPatch.length === 1 && jsonPatch[0].op === ADD && jsonPatch[0].path.length === 0) {
+            mutable = mutableObject(jsonPatch[0].value) as T;
             return [mutable, _deserialize(mutable)]
         }
         if (!mutable)
             mutable = mutableObject({}) as T;
-        if (serialized.length === 1 && serialized[0].path.length === 0 && serialized[0].op === REPLACE)
-            (mutable as MutableContract).setOriginal(serialized[0].value)
+        if (jsonPatch.length === 1 && jsonPatch[0].path.length === 0 && jsonPatch[0].op === REPLACE)
+            (mutable as MutableContract).setOriginal(jsonPatch[0].value)
         else
-            patch(mutable, serialized)
+            patch(mutable, jsonPatch)
         return [mutable, _deserialize(mutable)]
     }
 }
