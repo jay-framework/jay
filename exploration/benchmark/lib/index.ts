47bdae3373f2c6834a761d12cb6ff228407f295d
yoav <yoavaa@gmail.com>
2023-07-27 13:44:06 +0300

    benchmark comparing mutable to immutable deserialization, for now immutable wins bigtime

---
+++ exploration/benchmark/lib/index.ts
@@ -0,0 +1,76 @@
+import {mutableObject} from '../../../packages/mutable'
+import { REPLACE } from '../../../packages/mutable-contract';
+import {patchMutable} from "./patch-mutable";
+import {patchImmutable} from "./patch-immutable";
+
+const BENCHMARK_CYCLES = 1000;
+const TABLE_SIZE = 100;
+const PATCH_SIZE = 100;
+
+function initTable(tableSize){
+    let tableLines = []
+    for (let x = 0; x < tableSize; x++) {
+        tableLines[x] = {id: x, cell: []};
+        for (let y = 0; y < tableSize; y++) {
+            tableLines[x].cell[y] = {id: y, value: Math.round(Math.random()*100)};
+        }
+    }
+    return tableLines;
+}
+
+function createPatch(tableSize, patchSize) {
+    let patch = [];
+    for (let i = 0; i < patchSize; i++) {
+        let x = Math.floor(Math.random() * tableSize);
+        let y = Math.floor(Math.random() * tableSize);
+        patch.push({op: REPLACE, path: [''+x, 'cell', ''+y, 'value'], value: Math.round(Math.random() * 100)})
+    }
+    return patch;
+}
+
+
+function benchmarkMutable() {
+    let mutableTable = mutableObject(initTable(TABLE_SIZE));
+    let jsonPatch = createPatch(TABLE_SIZE, PATCH_SIZE);
+    patchMutable(mutableTable, jsonPatch)
+
+    console.log('creating patches')
+    let start = performance.now();
+    let patches = []
+    for (let i = 0; i < BENCHMARK_CYCLES; i++) {
+        patches[i] = createPatch(TABLE_SIZE, PATCH_SIZE);
+    }
+    console.log('start', start);
+    for (let i = 0; i < BENCHMARK_CYCLES; i++) {
+        patchMutable(mutableTable, patches[i])
+        mutableTable.freeze();
+    }
+    let end = performance.now();
+    console.log('end', end);
+    console.log('average time to apply patches to mutable', (end - start) / BENCHMARK_CYCLES);
+}
+
+function benchmarkImmutable() {
+    let table = initTable(TABLE_SIZE);
+    let jsonPatch = createPatch(TABLE_SIZE, PATCH_SIZE);
+    patchMutable(table, jsonPatch)
+
+    console.log('creating patches')
+    let start = performance.now();
+    let patches = []
+    for (let i = 0; i < BENCHMARK_CYCLES; i++) {
+        patches[i] = createPatch(TABLE_SIZE, PATCH_SIZE);
+    }
+    console.log('start', start);
+    for (let i = 0; i < BENCHMARK_CYCLES; i++) {
+        patchImmutable(table, patches[i])
+    }
+    let end = performance.now();
+    console.log('end', end);
+    console.log('average time to apply patches to immutable', (end - start) / BENCHMARK_CYCLES);
+}
+
+benchmarkMutable()
+benchmarkImmutable()
+benchmarkMutable()
+benchmarkImmutable()
---
+++ exploration/benchmark/lib/patch-immutable.ts
@@ -0,0 +1,60 @@
+import {ADD, JSONPatch, JSONPatchMove, JSONPatchOperation, MOVE, REMOVE, REPLACE} from "../../../packages/mutable-contract";
+
+function validateMove({from, path}: JSONPatchMove) {
+    let valid = (from.length === path.length)
+    for (let i=0, length = from.length-1; i < length; i++)
+        valid = valid && (from[i] === path[i])
+    return valid;
+}
+
+function applyPatchOperation(target: object, patchOperation: JSONPatchOperation) {
+    let {path} = patchOperation
+    let dirLength = path.length - 1;
+    for (let i = 0; i < dirLength; i++) {
+        target = target[path[i]];
+        if (!target)
+            return;
+    }
+    if (patchOperation.op === REPLACE || patchOperation.op === ADD)
+        target[path[dirLength]] = patchOperation.value
+    else if (patchOperation.op === REMOVE) {
+        if (Array.isArray(target))
+            target.splice(path[dirLength] as number, 1)
+        else
+            delete target[path[dirLength]];
+    }
+    else if (patchOperation.op === MOVE && Array.isArray(target) && validateMove(patchOperation)) {
+        let [item] = target.splice(patchOperation.from[dirLength] as number, 1)
+        target.splice(path[dirLength] as number, 0, item);
+    }
+}
+
+export function patchImmutable(target: object, patch: JSONPatch, level = 0) {
+    let copy = Array.isArray(target)?[...target]:{...target}
+    let patchesGroupedByProp = patch.reduce((prev, patchOperation) => {
+        if (patchOperation.path.length - 1 === level) {
+            if (patchOperation.op === REPLACE || patchOperation.op === ADD)
+                copy[patchOperation.path[level]] = patchOperation.value
+            else if (patchOperation.op === REMOVE) {
+                if (Array.isArray(copy))
+                    copy.splice(patchOperation.path[level] as number, 1)
+                else
+                    delete copy[patchOperation.path[level]];
+            }
+            else if (patchOperation.op === MOVE && Array.isArray(copy) && validateMove(patchOperation)) {
+                let [item] = copy.splice(patchOperation.from[level] as number, 1)
+                copy.splice(patchOperation.path[level] as number, 0, item);
+            }
+        }
+        else if (!prev[patchOperation.path[level]])
+            prev[patchOperation.path[level]] = [patchOperation]
+        else
+            prev[patchOperation.path[level]].push(patchOperation)
+        return prev;
+    }, {})
+
+    Object.keys(patchesGroupedByProp).forEach(key => {
+        copy[key] = patchImmutable(copy[key], patchesGroupedByProp[key], level+1)
+    })
+    return copy;
+}
