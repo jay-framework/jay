72c080a95bd2e7efdf3e5c4d82d5406f70f25d8a
yoav <yoavaa@gmail.com>
2023-08-01 15:00:01 +0300

    export diff and patch functions

--- packages/serialization/lib/index.ts
+++ packages/serialization/lib/index.ts
@@ -1,2 +1,4 @@
 export {deserialize, Deserialize} from './mutable-deserializer'
 export {serialize, Serialize} from './mutable-serializer'
+export {diff} from './serialize/diff'
+export {patch} from './deserialize/patch'
