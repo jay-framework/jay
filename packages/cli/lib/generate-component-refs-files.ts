963c87a650a8c73510e17cd47b92d12674335c58
yoav <yoavaa@gmail.com>
2023-09-21 10:18:06 +0300

    fixes in exports and files distribution due to the refactoring in compiler

--- packages/cli/lib/generate-component-refs-files.ts
+++ packages/cli/lib/generate-component-refs-files.ts
@@ -1,19 +1,19 @@
-import {parseJayFile} from "jay-compiler";
-import {JayImportLink} from "jay-compiler/dist/parse-jay-file";
+import { parseJayFile } from 'jay-compiler';
+import { JayImportLink } from 'jay-compiler';
 
 export interface CollectedImport {
-    fileName: string
-    filePath: string
-    link: JayImportLink
+    fileName: string;
+    filePath: string;
+    link: JayImportLink;
 }
 
 export function generateRefsForImportedComponents(importsCollector: Array<CollectedImport>) {
     return function (html: string, fileName: string, filePath: string) {
-        let parsedFile = parseJayFile(html, fileName, filePath)
+        let parsedFile = parseJayFile(html, fileName, filePath);
         console.log('generate refs for ', fileName, filePath);
         if (parsedFile.validations.length === 0) {
-            let jayFile = parsedFile.val
-            jayFile.imports.map(link => importsCollector.push({fileName, filePath, link}));
+            let jayFile = parsedFile.val;
+            jayFile.imports.map((link) => importsCollector.push({ fileName, filePath, link }));
         }
-    }
+    };
 }
--- packages/compiler/lib/index.ts
+++ packages/compiler/lib/index.ts
@@ -1,4 +1,5 @@
-export * from './jay-file/jay-file-compiler'
-export {parseJayFile} from './jay-file/jay-file-parser';
-export {WithValidations} from "./core/with-validations";
-export {generateRefsFile} from './ts-file/ts-refs-file-generator'
+export * from './jay-file/jay-file-compiler';
+export { parseJayFile } from './jay-file/jay-file-parser';
+export { WithValidations } from './core/with-validations';
+export { generateRefsFile } from './ts-file/ts-refs-file-generator';
+export { JayImportLink } from './core/jay-file-types';
