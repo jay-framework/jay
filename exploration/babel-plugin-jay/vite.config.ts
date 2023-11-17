fd946491d0cf3490ccc1d17a8676c4373cbe3c48
Karolis Astrauka <astrauka@gmail.com>
2023-11-17 09:46:56 +0200

    [migrate] babel plugin jay to vite

---
+++ exploration/babel-plugin-jay/vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    build: {
+        minify: false,
+        target: 'es2020',
+        ssr: resolve(__dirname, 'lib/index.ts'),
+        lib: {
+            entry: resolve(__dirname, 'lib/index.ts'),
+            name: 'jsonPatch',
+            fileName: 'index',
+            formats: ['cjs'],
+        },
+    },
+    test: {
+        globals: true,
+        setupFiles: 'jay-dev-environment/library/vitest.setup.ts',
+    },
+});
