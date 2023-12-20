c1c4a850615dc0ff071cd227f6d54272ae02ab5c
Karolis Astrauka <astrauka@gmail.com>
2023-12-20 13:45:24 +0200

    [compiler] compiler transforms code to jayFile to code
    Transform code to JayFile, use it in code generation.
    Use originId to tell the origin file.
    Simplify load file logic.
    Resolve correct originId for Vite sandbox watch mode.

--- packages/rollup-plugin/lib/runtime/generate-code-from-structure.ts
+++ packages/rollup-plugin/lib/runtime/generate-code-from-structure.ts
@@ -8,7 +8,6 @@ import {
     generateElementBridgeFile,
     generateElementFile,
     generateImportsFileFromJayFile,
-    generateImportsFileFromTsSource,
     generateSandboxRootFile,
     getModeFromExtension,
     JayFile,
