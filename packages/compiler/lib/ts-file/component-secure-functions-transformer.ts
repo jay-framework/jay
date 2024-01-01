8b587925e16d039f9714d93c12a87a1aa0a34f86
yoav <yoavaa@gmail.com>
2024-01-01 11:13:38 +0200

    added the transform event handler logic inline. To be extracted to a separate transformer block

--- packages/compiler/lib/ts-file/component-secure-functions-transformer.ts
+++ packages/compiler/lib/ts-file/component-secure-functions-transformer.ts
@@ -1,15 +1,35 @@
-import ts, {isFunctionDeclaration} from 'typescript';
+import ts, {
+    isBlock,
+    isCallExpression,
+    isExpression,
+    isFunctionDeclaration,
+    isPropertyAccessExpression
+} from 'typescript';
 import { mkTransformer, SourceFileTransformerContext } from './mk-transformer';
 import { findMakeJayComponentImportTransformerBlock } from './building-blocks/find-make-jay-component-import';
 import { findComponentConstructorsBlock } from './building-blocks/find-component-constructors.ts';
 import { findComponentConstructorCallsBlock } from './building-blocks/find-component-constructor-calls.ts';
 import { findEventHandlersBlock } from './building-blocks/find-event-handler-functions.ts';
-import {NameBindingResolver} from "./building-blocks/name-binding-resolver.ts";
+import {flattenVariable, NameBindingResolver} from "./building-blocks/name-binding-resolver.ts";
 import {compileFunctionSplitPatternsBlock} from "./building-blocks/compile-function-split-patterns.ts";
 
 type ComponentSecureFunctionsTransformerConfig = SourceFileTransformerContext & {
     patterns: string[];
 };
+
+const transformEventHandlerStatement: (nameBindingResolver: NameBindingResolver) => ts.Visitor =
+    (nameBindingResolver: NameBindingResolver) => (node) => {
+    if (isCallExpression(node)) {
+        node.arguments.forEach(argument => {
+            if (isPropertyAccessExpression(argument)) {
+                let resolvedParam = nameBindingResolver.resolvePropertyAccessChain(argument);
+                console.log(flattenVariable(resolvedParam));
+            }
+        })
+
+    }
+    return node;
+}
 
 function mkComponentSecureFunctionsTransformer(
     sftContext: ComponentSecureFunctionsTransformerConfig,
@@ -21,13 +41,21 @@ function mkComponentSecureFunctionsTransformer(
     let calls = findComponentConstructorCallsBlock(makeJayComponent_ImportName, sftContext);
     let constructorExpressions = calls.map(({ comp }) => comp);
     let constructorDefinitions = findComponentConstructorsBlock(constructorExpressions, sftContext);
-    let eventHandlers = constructorDefinitions.map((constructorDefinition) =>
+    let eventHandlers = constructorDefinitions.flatMap((constructorDefinition) =>
         findEventHandlersBlock(constructorDefinition, sftContext),
     );
 
     // compile patterns
     let {patterns, context} = sftContext;
     let compiledPatterns = compileFunctionSplitPatternsBlock(patterns);
+
+    eventHandlers.forEach(eventHandler => {
+        let nameBindingResolver = new NameBindingResolver();
+        nameBindingResolver.addFunctionParams(eventHandler);
+
+        ts.visitEachChild(eventHandler, transformEventHandlerStatement(nameBindingResolver), context)
+
+    });
 
     // todo start transforming the component definition functions
 
