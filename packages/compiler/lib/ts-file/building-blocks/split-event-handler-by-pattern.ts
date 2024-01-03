092da34b3c29c1051218540a31545425d9457528
yoav <yoavaa@gmail.com>
2024-01-03 11:26:43 +0200

    first test for split-event-handler-by-pattern working

--- packages/compiler/lib/ts-file/building-blocks/split-event-handler-by-pattern.ts
+++ packages/compiler/lib/ts-file/building-blocks/split-event-handler-by-pattern.ts
@@ -1,26 +1,40 @@
-import ts, {isCallExpression, isPropertyAccessExpression} from "typescript";
-import {flattenVariable, NameBindingResolver} from "./name-binding-resolver.ts";
+import ts, {Expression, ExpressionStatement, isCallExpression, isPropertyAccessExpression} from "typescript";
+import {FlattenedAccessChain, flattenVariable, NameBindingResolver} from "./name-binding-resolver.ts";
 import {CompiledPattern} from "./compile-function-split-patterns.ts";
+import {astToCode, codeToAst} from "../ts-compiler-utils.ts";
 
-const transformEventHandlerStatement: (nameBindingResolver: NameBindingResolver) => ts.Visitor =
-    (nameBindingResolver: NameBindingResolver) => (node) => {
+function findPatternInVariable(resolvedParam: FlattenedAccessChain, paramIndex: number, compiledPatterns: CompiledPattern[]) {
+    return compiledPatterns.find(pattern =>
+        (paramIndex === pattern.paramIndex &&
+            pattern.accessChain.path.length <= resolvedParam.path.length &&
+            pattern.accessChain.path.every((element, index) => element = resolvedParam.path[index]))
+    )
+}
+
+const transformEventHandlerStatement =
+    (nameBindingResolver: NameBindingResolver, compiledPatterns: CompiledPattern[], context: ts.TransformationContext, factory: ts.NodeFactory) => (node) => {
         if (isCallExpression(node)) {
-            node.arguments.forEach(argument => {
+            let newArguments: Expression[] = node.arguments.map((argument, paramIndex) => {
                 if (isPropertyAccessExpression(argument)) {
                     let resolvedParam = nameBindingResolver.resolvePropertyAccessChain(argument);
-                    console.log(flattenVariable(resolvedParam));
+                    let flattenedResolvedParam = flattenVariable(resolvedParam);
+                    let patternMatch = findPatternInVariable(flattenedResolvedParam, paramIndex, compiledPatterns);
+                    let replacementPattern = ['event.$1', ...flattenedResolvedParam.path.splice(patternMatch.accessChain.path.length)];
+                    return (codeToAst(replacementPattern.join('.'), context)[0] as ExpressionStatement).expression
                 }
+                else
+                    return argument;
             })
-
+            return factory.createCallExpression(node.expression, undefined, newArguments)
         }
         return node;
     }
 
-export const splitEventHandlerByPatternBlock: (context: ts.TransformationContext, compiledPatterns: CompiledPattern[]) => ts.Visitor =
-    (context: ts.TransformationContext) => (eventHandler: ts.FunctionLikeDeclarationBase) => {
+export const splitEventHandlerByPatternBlock =
+    (context: ts.TransformationContext, compiledPatterns: CompiledPattern[], factory: ts.NodeFactory) => (eventHandler: ts.FunctionLikeDeclarationBase) => {
 
-    let nameBindingResolver = new NameBindingResolver();
-    nameBindingResolver.addFunctionParams(eventHandler);
+    let eventHandlerNameResolver = new NameBindingResolver();
+    eventHandlerNameResolver.addFunctionParams(eventHandler);
 
-    return ts.visitEachChild(eventHandler, transformEventHandlerStatement(nameBindingResolver), context)
+    return ts.visitEachChild(eventHandler, transformEventHandlerStatement(eventHandlerNameResolver, compiledPatterns, context, factory), context)
 }
--- packages/compiler/test/ts-building-blocks/split-event-handler-by-pattern.test.ts
+++ packages/compiler/test/ts-building-blocks/split-event-handler-by-pattern.test.ts
@@ -5,8 +5,8 @@ import {
 import {mkTransformer} from "../../lib/ts-file/mk-transformer.ts";
 import {splitEventHandlerByPatternBlock} from "../../lib/ts-file/building-blocks/split-event-handler-by-pattern.ts";
 import {transformCode} from "../test-utils/ts-compiler-test-utils.ts";
-import {isExpressionStatement} from "typescript";
-import {isFunctionLikeDeclarationBase} from "../../lib/ts-file/ts-compiler-utils.ts";
+import ts, {isExpressionStatement} from "typescript";
+import {prettify} from "../../lib";
 
 
 const PATTERN_EVENT_TARGET_VALUE = `
@@ -18,11 +18,14 @@ describe('split event handler by pattern', () => {
     const patterns = compileFunctionSplitPatternsBlock([PATTERN_EVENT_TARGET_VALUE])
 
     function testTransformer(compiledPatterns: CompiledPattern[]) {
-        return mkTransformer(({context, sourceFile}) => {
-            if (isExpressionStatement(sourceFile.statements[0]) &&
-                isFunctionLikeDeclarationBase(sourceFile.statements[0].expression))
-            splitEventHandlerByPatternBlock(context, compiledPatterns)(sourceFile.statements[0].expression);
-            return sourceFile;
+        return mkTransformer(({context, sourceFile, factory}) => {
+
+            return ts.visitEachChild(sourceFile, (statement) => {
+                if (isExpressionStatement(statement)) {
+                    return ts.visitEachChild(statement, splitEventHandlerByPatternBlock(context, compiledPatterns, factory), context);
+                }
+                return statement;
+            }, context);
         })
     }
 
@@ -31,6 +34,8 @@ describe('split event handler by pattern', () => {
             `({event}) => setText((event.target as HTMLInputElement).value)`
         const transformerState = testTransformer(patterns.val);
         let transformed = await transformCode(inputEventHandler, [transformerState]);
+
+        expect(transformed).toEqual(await prettify(`({event}) => setText(event.$1)`));
         console.log(transformed);
 
     })
