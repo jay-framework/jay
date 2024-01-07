902e0aad851f3f4d03ffff293a0c88dff882f1f6
yoav <yoavaa@gmail.com>
2024-01-07 11:21:03 +0200

    added the transformer block that add the call$ to the event handlers chain

---
+++ packages/compiler/lib/ts-file/building-blocks/add-event-handler-call$.ts
@@ -0,0 +1,36 @@
+import ts, {
+    CallExpression, Expression, ExpressionStatement,
+    isCallExpression,
+    isPropertyAccessExpression,
+    NodeFactory,
+    TransformationContext
+} from "typescript";
+import {codeToAst} from "../ts-compiler-utils.ts";
+
+const addEventHandlerCall = (context: TransformationContext, factory: NodeFactory) => (node) => {
+    if (isCallExpression(node) && isPropertyAccessExpression(node.expression)) {
+        node.expression.name
+        node.expression.expression
+        node.arguments
+        return factory.createCallExpression(
+            factory.createPropertyAccessExpression(
+                factory.createCallExpression(
+                    factory.createPropertyAccessExpression(
+                        node.expression.expression,
+                        node.expression.name.text + '$'
+                    ),
+                    undefined,
+                    codeToAst(`handler$('1')`, context).map((_:ExpressionStatement) => _.expression) as Expression[]
+                ),
+                node.expression.name),
+            undefined,
+            node.arguments
+        )
+    }
+    return node;
+}
+
+export const addEventHandlerCallBlock =
+    (context: TransformationContext, factory: NodeFactory) => (eventHandler: CallExpression) => {
+        return ts.visitNode(eventHandler, addEventHandlerCall(context, factory))
+    }
---
+++ packages/compiler/test/ts-building-blocks/add-event-handler-call$.test.ts
@@ -0,0 +1,34 @@
+
+import {mkTransformer} from "../../lib/ts-file/mk-transformer.ts";
+import ts, {isCallExpression, isExpressionStatement, isFunctionDeclaration} from "typescript";
+import {transformCode} from "../test-utils/ts-compiler-test-utils.ts";
+import {prettify} from "../../lib";
+import {addEventHandlerCallBlock} from "../../lib/ts-file/building-blocks/add-event-handler-call$.ts";
+
+
+describe('add event handler call$ to call chain', () => {
+
+    function testTransformer() {
+        return mkTransformer(({context, sourceFile, factory}) => {
+
+            return ts.visitEachChild(sourceFile, (statement) => {
+                if (isExpressionStatement(statement) && isCallExpression(statement.expression)) {
+                    return ts.visitNode(statement.expression, addEventHandlerCallBlock(context, factory));
+                }
+                return statement;
+            }, context);
+        })
+    }
+
+    it('should support inline event handler', async () => {
+        const eventHandlerCall =
+            `refs.comp.onclick(({event}) => {})`
+        const transformerState = testTransformer();
+        let transformed = await transformCode(eventHandlerCall, [transformerState]);
+
+        expect(transformed).toEqual(await prettify(`refs.comp.onclick$(handler$('1')).onclick(({event}) => {})`));
+        console.log(transformed);
+
+    })
+
+});
