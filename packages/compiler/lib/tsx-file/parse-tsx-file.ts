24da0bdd508fcc540f86285e0b7d2c069e097e71
Karolis Astrauka <astrauka@gmail.com>
2024-01-12 15:42:53 +0200

    [refactor] find component constructor import name from import links

--- packages/compiler/lib/tsx-file/parse-tsx-file.ts
+++ packages/compiler/lib/tsx-file/parse-tsx-file.ts
@@ -1,29 +1,31 @@
 import { JayFile } from '../core/jay-file-types';
 import { WithValidations } from '../core/with-validations';
-import { parseImportLinks } from '../ts-file/parse-jay-file/parse-import-links';
+import { getImportByName, parseImportLinks } from '../ts-file/parse-jay-file/parse-import-links';
 import { createTsSourceFileFromSource } from '../ts-file/building-blocks/create-ts-source-file-from-source';
 import { findComponentConstructorCallsBlock } from '../ts-file/building-blocks/find-component-constructor-calls';
 import { getBaseElementName } from '../ts-file/building-blocks/get-base-element-name';
-import { findMakeJayComponentImportTransformerBlock } from '../ts-file/building-blocks/find-make-jay-component-import';
-import { MAKE_JAY_TSX_COMPONENT } from '../core/constants';
+import { JAY_COMPONENT, MAKE_JAY_TSX_COMPONENT } from '../core/constants';
 
 export function parseTsxFile(filename: string, source: string): WithValidations<JayFile> {
     const sourceFile = createTsSourceFileFromSource(filename, source);
     const imports = parseImportLinks(sourceFile);
-    let makeJayComponent_ImportName = findMakeJayComponentImportTransformerBlock(
+    const makeJayTsxComponentImport = getImportByName(
+        imports,
+        JAY_COMPONENT,
         MAKE_JAY_TSX_COMPONENT,
-        sourceFile,
     );
-    if (!Boolean(makeJayComponent_ImportName))
+    if (!Boolean(makeJayTsxComponentImport))
         return new WithValidations<JayFile>(undefined, [
             `Missing ${MAKE_JAY_TSX_COMPONENT} import`,
         ]);
 
+    const makeJayTsxComponent_ImportName =
+        makeJayTsxComponentImport.as || makeJayTsxComponentImport.name;
     const componentConstructors = findComponentConstructorCallsBlock(
-        makeJayComponent_ImportName,
+        makeJayTsxComponent_ImportName,
         sourceFile,
     );
-    return getBaseElementName(makeJayComponent_ImportName, componentConstructors).map(
+    return getBaseElementName(makeJayTsxComponent_ImportName, componentConstructors).map(
         (baseElementName) =>
             ({
                 imports,
--- packages/compiler/test/ts-building-blocks/parse-import-links.test.ts
+++ packages/compiler/test/ts-building-blocks/parse-import-links.test.ts
@@ -1,5 +1,8 @@
-import { createTsSourceFileFromSource, JayUnknown } from '../../lib';
-import { parseImportLinks } from '../../lib/ts-file/parse-jay-file/parse-import-links';
+import { createTsSourceFileFromSource, JayImportLink, JayUnknown } from '../../lib';
+import {
+    getImportByName,
+    parseImportLinks,
+} from '../../lib/ts-file/parse-jay-file/parse-import-links';
 import { readFixtureFile } from '../test-utils/ts-compiler-test-utils';
 
 describe('parseImportLinks', () => {
@@ -73,5 +76,56 @@ describe('parseImportLinks', () => {
                 sandbox: true,
             },
         ]);
+    });
+});
+
+describe('getImportByName', () => {
+    const importLinks: JayImportLink[] = [
+        {
+            module: 'jay-runtime',
+            names: [
+                {
+                    name: 'JayElement',
+                    type: JayUnknown,
+                },
+                {
+                    as: 'e',
+                    name: 'element',
+                    type: JayUnknown,
+                },
+            ],
+            sandbox: false,
+        },
+        {
+            module: 'jay-secure',
+            names: [
+                {
+                    as: 'mr',
+                    name: 'mainRoot',
+                    type: JayUnknown,
+                },
+                {
+                    name: 'secureChildComp',
+                    type: JayUnknown,
+                },
+            ],
+            sandbox: false,
+        },
+    ];
+    const component = 'jay-runtime';
+    const name = 'element';
+
+    it('returns import by name', () => {
+        const importName = getImportByName(importLinks, component, name);
+        expect(importName).toEqual(importLinks[0].names[1]);
+    });
+
+    describe('on no import by name', () => {
+        const name = 'other';
+
+        it('returns undefined', () => {
+            const importName = getImportByName(importLinks, component, name);
+            expect(importName).toBeUndefined();
+        });
     });
 });
