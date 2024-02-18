f8c4cb1b95034bee5ba06cd2e690aeaa90e3cd11
yoav <yoavaa@gmail.com>
2024-02-18 14:46:46 +0200

    statement dependencies calculation

---
+++ packages/compiler/lib/ts-file/building-blocks/source-file-statement-dependencies.ts
@@ -0,0 +1,150 @@
+import ts, {
+    Block,
+    isBlock,
+    isIdentifier,
+    isIfStatement, isPropertyAccessChain, isPropertyAccessExpression,
+    isSourceFile, isStatement,
+    isVariableStatement, Node, NodeArray,
+    SourceFile,
+    Statement
+} from "typescript";
+import {NameBindingResolver} from "./name-binding-resolver";
+import {SourceFileBindingResolver} from "./source-file-binding-resolver.ts";
+
+// export class DAGStatement {
+//     constructor(
+//         public readonly id: string,
+//         public readonly statement: Statement | SourceFile,
+//         public readonly childDAGs: Map<ts.Node, DAG>,
+//         public readonly dependsOn: DAGStatement[],
+//         public readonly isDependencyFor: DAGStatement[],
+//     ) {}
+// }
+//
+// export class DAG extends DAGStatement { //todo DAG is not a statement, but a node.
+//     constructor(
+//         id: string, // todo remove
+//         statement: Statement | SourceFile, // todo change to node
+//         childDAGs: Map<ts.Node, DAG>, // todo remove
+//         dependsOn: DAGStatement[], // todo remove
+//         isDependencyFor: DAGStatement[], // todo remove
+//         public readonly nameBindingResolver: NameBindingResolver,
+//         public readonly childStatements: DAGStatement[]
+//     ) {
+//         super(id, statement, childDAGs, dependsOn, isDependencyFor)
+//     }
+// }
+//
+// // isSafe is on expression
+// // DAG is on statements
+//
+// function id(parentId: string, index: number) {
+//     if (parentId)
+//         return `${parentId}.${index}`;
+//     else
+//         return `${index}`;
+// }
+//
+// function findIdentifierDependencies(statement: Statement, nameBindingResolver: NameBindingResolver): string[] {
+//     let dependencies = [];
+//     let visitorNode = (node: ts.Node) => {
+//         if (isStatement(node))
+//             return node;
+//         if (isPropertyAccessExpression(node))
+//             visitorNode(node.expression);
+//         else if (isIdentifier(node)) {
+//             if (nameBindingResolver.getVariable(node.text))
+//                 dependencies.push(node.text);
+//         }
+//         else
+//             node.getChildren().forEach(childNode => ts.visitNode(childNode, visitorNode))
+//         return node;
+//     }
+//
+//     statement.getChildren().forEach(childNode => ts.visitNode(childNode, visitorNode))
+//     return dependencies;
+// }
+//
+// function resolveDAGStatementDependencies(dependsOnNames: string[], astToDag: Map<ts.Statement, DAGStatement>, nameBindingResolver: NameBindingResolver, thisStatement: ts.Statement): DAGStatement[] {
+//     return dependsOnNames
+//         .map(dependsOnName => nameBindingResolver.getVariable(dependsOnName))
+//         .filter(variable => !!variable.name && variable.definingStatement !== thisStatement)
+//         .map(variable => astToDag.get(variable.definingStatement))
+// }
+//
+// export function createDAG(blockOrFile: Block | SourceFile, parentId: string = ''): DAG {
+//     let nameBindingResolver = new NameBindingResolver();
+//     let astToDag = new Map<Statement, DAGStatement>();
+//     let childStatements =
+//         blockOrFile.statements.map((thisStatement, index) => {
+//             let childDAGs = new Map<ts.Node, DAG>();
+//             if (isVariableStatement(thisStatement))
+//                 nameBindingResolver.addVariableStatement(thisStatement);
+//             let dependsOnNames = findIdentifierDependencies(thisStatement, nameBindingResolver);
+//             let dependsOn = resolveDAGStatementDependencies(dependsOnNames, astToDag, nameBindingResolver, thisStatement);
+//
+//             const dagStatement = new DAGStatement(id(parentId, index), thisStatement, childDAGs, dependsOn, []);
+//             dependsOn.forEach(dependsOnStatement => dependsOnStatement.isDependencyFor.push(dagStatement));
+//             astToDag.set(thisStatement, dagStatement);
+//             return dagStatement;
+//         });
+//     return new DAG(parentId, blockOrFile, childDAGs, [], [], nameBindingResolver, childStatements);
+// }
+
+export interface StatementDependencies {
+    id: number,
+    parent?: Statement;
+    statement: Statement;
+    dependsOn: Set<StatementDependencies>,
+    isDependencyFor: Set<StatementDependencies>,
+}
+
+
+export class SourceFileStatementDependencies {
+    private statementDependencies = new Map<ts.Node, StatementDependencies>();
+
+    constructor(sourceFile: SourceFile, bindingResolver: SourceFileBindingResolver) {
+        let parents: StatementDependencies[] = []
+        let id = 0;
+        const visitor = (node: ts.Node): ts.Node =>  {
+            if (isStatement(node)) {
+                const statementDependencies = {
+                    statement: node,
+                    parent: parents.at(-1)?.statement,
+                    isDependencyFor: new Set<StatementDependencies>(),
+                    dependsOn: new Set<StatementDependencies>(),
+                    id: id++
+                };
+                this.statementDependencies.set(node, statementDependencies)
+                parents.push(statementDependencies);
+            }
+            if (isIdentifier(node)) {
+                let variable = bindingResolver.explain(node);
+                let thisStatementDependencies = parents.at(-1);
+                if (variable.definingStatement && variable.definingStatement !== thisStatementDependencies.statement) {
+                    let dependsOnStatementDependencies = this.statementDependencies.get(variable.definingStatement);
+                    thisStatementDependencies.dependsOn.add(dependsOnStatementDependencies);
+                    dependsOnStatementDependencies.isDependencyFor.add(thisStatementDependencies);
+
+                }
+            }
+
+            node.getChildren().forEach(child =>
+                ts.visitNode(child, visitor))
+            return node;
+        }
+        ts.visitNode(sourceFile, visitor)
+    }
+
+    getDependsOn(statement: Statement) {
+        return this.statementDependencies.get(statement).dependsOn;
+    }
+
+    getIsDependencyFor(statement: Statement) {
+        return this.statementDependencies.get(statement).isDependencyFor;
+    }
+
+    getAllStatements() {
+        return this.statementDependencies.values();
+    }
+}
---
+++ packages/compiler/test/ts-building-blocks/source-file-statement-dependencies.test.ts
@@ -0,0 +1,109 @@
+import {createTsSourceFile} from "../test-utils/ts-source-utils.ts";
+import {astToFormattedCode} from "../test-utils/ts-compiler-test-utils.ts";
+import {SourceFileStatementDependencies} from "../../lib/ts-file/building-blocks/source-file-statement-dependencies.ts";
+import {SourceFileBindingResolver} from "../../lib/ts-file/building-blocks/source-file-binding-resolver.ts";
+import {prettify} from "../../lib";
+
+interface PrintedStatement {
+    id: number,
+    statement: string,
+    dependencies: string
+}
+async function print(dag: SourceFileStatementDependencies): Promise<Set<PrintedStatement>> {
+    let printedStatements = new Set<PrintedStatement>();
+    for await (let statementDependencies of dag.getAllStatements()) {
+        let dependsOn = [...statementDependencies.dependsOn].map(dependsOn => `this -> ${dependsOn.id}`)
+        let dependencyFor = [...statementDependencies.isDependencyFor].map(dependencyFor => `this <- ${dependencyFor.id}`)
+        let dependencies = [...dependsOn, ...dependencyFor].sort().join(', ')
+        let statement = (await astToFormattedCode(statementDependencies.statement)).trim();
+        printedStatements.add({id: statementDependencies.id, statement, dependencies})
+    }
+    return printedStatements;
+}
+
+async function format(code) {
+    return (await prettify(code)).trim();
+}
+
+describe('statement-DAG', () => {
+
+    describe('support flat statement dependencies', () => {
+        it('should create DAG for line by line dependencies', async () => {
+            const sourceFile = createTsSourceFile(`
+            let x = 10;
+            let y = x + 1;
+            console.log(y)`);
+            let bindingResolver = new SourceFileBindingResolver(sourceFile);
+            let statementDependencies = new SourceFileStatementDependencies(sourceFile, bindingResolver);
+
+            expect(await print(statementDependencies)).toEqual(new Set([
+                {id: 0, statement: 'let x = 10;', dependencies: 'this <- 1'},
+                {id: 1, statement: 'let y = x + 1;', dependencies: 'this -> 0, this <- 2'},
+                {id: 2, statement: 'console.log(y);', dependencies: 'this -> 1'}
+            ]))
+        })
+
+        it('should create DAG for line by line dependencies', async () => {
+            const sourceFile = createTsSourceFile(`
+            let x = 10;
+            let z = 20;
+            let y = x + z;
+            console.log(x, y)`);
+            let bindingResolver = new SourceFileBindingResolver(sourceFile);
+            let statementDependencies = new SourceFileStatementDependencies(sourceFile, bindingResolver);
+
+            expect(await print(statementDependencies)).toEqual(new Set([
+                {id: 0, statement: 'let x = 10;', dependencies: 'this <- 2, this <- 3'},
+                {id: 1, statement: 'let z = 20;', dependencies: 'this <- 2'},
+                {id: 2, statement: 'let y = x + z;', dependencies: 'this -> 0, this -> 1, this <- 3'},
+                {id: 3, statement: 'console.log(x, y);', dependencies: 'this -> 0, this -> 2'}
+            ]))
+        })
+
+    })
+
+    describe('support import dependencies', () => {
+
+    });
+
+    describe("support function statement dependencies (params to body and in-body dependencies)", () => {
+
+        it('should create DAG for a function declaration', async () => {
+            const sourceFile = createTsSourceFile(`function bla ({ event }) {
+                const inputValue = event.target.value;
+                const validValue = inputValue.replace(/[^A-Za-z0-9]+/g, '');
+                event.target.value = validValue;
+            }`);
+            let bindingResolver = new SourceFileBindingResolver(sourceFile);
+            let statementDependencies = new SourceFileStatementDependencies(sourceFile, bindingResolver);
+
+            expect(await print(statementDependencies)).toEqual(new Set([
+                {
+                    id: 0,
+                    dependencies: "this <- 1, this <- 3",
+                    statement: await format(`function bla({ event }) {
+                        const inputValue = event.target.value;
+                        const validValue = inputValue.replace(/[^A-Za-z0-9]+/g, '');
+                        event.target.value = validValue;
+                    }`),
+                },
+                {
+                    id: 1,
+                    dependencies: "this -> 0, this <- 2",
+                    statement: "const inputValue = event.target.value;",
+                },
+                {
+                    id: 2,
+                    dependencies: "this -> 1, this <- 3",
+                    statement: "const validValue = inputValue.replace(/[^A-Za-z0-9]+/g, '');",
+                },
+                {
+                    id: 3,
+                    dependencies: "this -> 0, this -> 2",
+                    statement: "event.target.value = validValue;",
+                },
+            ]))
+        })
+
+    })
+});
