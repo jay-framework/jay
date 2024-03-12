f96d4a4832a089eb9d5f85df03b0df57dbdab717
yoav <yoavaa@gmail.com>
2024-03-12 14:17:13 +0200

    set the todo list as sandboxed and added sandbox root

--- examples/todo-components/lib/index.ts
+++ examples/todo-components/lib/index.ts
@@ -1,5 +1,6 @@
 import { render } from './app.jay-html';
 import './index.css';
+import {HandshakeMessageJayChannel, JayPort, setMainPort} from "jay-secure";
 
 const initialTodos = [
     {
@@ -19,7 +20,12 @@ const initialTodos = [
     },
 ];
 
+const jayWorker = new Worker(new URL('jay-sandbox:./sandbox-root', import.meta.url), {
+    type: 'module',
+});
+
 window.onload = function () {
+    setMainPort(new JayPort(new HandshakeMessageJayChannel(jayWorker)));
     let target = document.getElementById('target');
 
     let instance = render({ todoProps: {initialTodos} });
