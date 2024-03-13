22e0ab8d96d8cd5bc90905ca2401b3471550057e
yoav <yoavaa@gmail.com>
2024-03-13 16:50:40 +0200

    removed unneeded code

--- exploration/compiler-jsx/lib/jay-file.ts
+++
@@ -1,7 +0,0 @@
-export function h(element, properties, ...children) {
-    return `${element} ${JSON.stringify(properties)} \n` + children.join('\n');
-}
-
-export const jayFile = <T>(props: T, ...children) => {
-    return `...`;
-};
