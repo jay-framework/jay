1e3c8185bbba975eb4a9cb6f66007834a86b0c19
yoav <yoavaa@gmail.com>
2024-09-23 17:28:31 +0300

    created find exec$ TS building block

---
+++ packages/compiler/lib/ts-file/building-blocks/find-exec$.ts
@@ -0,0 +1,24 @@
+import ts from 'typescript';
+import {SourceFileBindingResolver} from "./source-file-binding-resolver";
+import {flattenVariable, isImportModuleVariableRoot} from "./name-binding-resolver";
+
+export function findExec$(bindingResolver: SourceFileBindingResolver, sourceFile: ts.SourceFile) {
+    const foundExec$ = [];
+
+    function visit(node: ts.Node) {
+        if (ts.isCallExpression(node) && ts.isIdentifier(node.expression)) {
+            const functionVariable = bindingResolver.explain(node.expression)
+            const accessChain = flattenVariable(functionVariable);
+            if (accessChain.path.length === 1 &&
+                accessChain.path[0] === 'exec$' &&
+                isImportModuleVariableRoot(accessChain.root) &&
+                ts.isStringLiteral(accessChain.root.module) &&
+                accessChain.root.module.text === 'jay-secure')
+            foundExec$.push(node);
+        }
+        ts.forEachChild(node, visit);
+    }
+
+    ts.forEachChild(sourceFile, visit);
+    return foundExec$;
+}
