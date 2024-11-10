0cd476cd6ed349ba56a8ab71f5c447165b8b4f0f
yoav <yoavaa@gmail.com>
2024-11-10 23:35:26 +0400

    removed dead code from todo regular example

--- examples/jay/todo/lib/sandbox-root.ts
+++
@@ -1,4 +0,0 @@
-import './app.jay-html';
-import { HandshakeMessageJayChannel, JayPort, setMainPort, setWorkerPort } from 'jay-secure';
-
-setWorkerPort(new JayPort(new HandshakeMessageJayChannel(this)));
