3a79cb386ed6d27cf7397533cffea7d3a8bebdfe
yoav <yoavaa@gmail.com>
2024-11-17 17:43:06 +0200

    added generic type assertions

--- packages/runtime/serialization/lib/mutable-serializer.ts
+++ packages/runtime/serialization/lib/mutable-serializer.ts
@@ -1,16 +1,22 @@
 import { ArrayContexts, diff } from 'jay-json-patch';
 import { JSONPatch } from 'jay-json-patch';
 
-export type Serialize = (entity: any, contexts?: ArrayContexts) => [JSONPatch, Serialize];
+export type Serialize<T extends object> = (
+    entity: any,
+    contexts?: ArrayContexts,
+) => [JSONPatch, Serialize<T>];
 
-export function serialize(entity: any, contexts?: ArrayContexts): [JSONPatch, Serialize] {
+export function serialize<T extends object>(
+    entity: any,
+    contexts?: ArrayContexts,
+): [JSONPatch, Serialize<T>] {
     return _serialize(undefined, contexts)(entity);
 }
 
-export function _serialize<T>(
+export function _serialize<T extends object>(
     lastEntity: T,
     contexts?: ArrayContexts,
-): (entity: T) => [JSONPatch, Serialize] {
+): (entity: T) => [JSONPatch, Serialize<T>] {
     return (entity: T) => {
         let patch = diff(entity, lastEntity, contexts);
         return [patch[0], _serialize(entity, contexts)];
