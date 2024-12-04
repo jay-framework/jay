13dc2c2e4334a1aab4c4bb15a2f9a5395c830095
yoav <yoavaa@gmail.com>
2024-12-04 11:53:00 +0200

    large number of components example

--- examples/jay/large-number-of-components/src/counter.ts
+++ examples/jay/large-number-of-components/src/counter.ts
@@ -1,19 +1,31 @@
 import { render, CounterElementRefs } from './counter.jay-html';
-import { createSignal, makeJayComponent, Props } from 'jay-component';
+import { createEvent, createSignal, makeJayComponent, Props } from 'jay-component';
 
 export interface CounterProps {
     initialValue: number;
     counterClass: string;
 }
 
-function CounterConstructor({ initialValue, counterClass }: Props<CounterProps>, refs: CounterElementRefs) {
+export interface CounterEvent {
+    value: number;
+}
+
+function CounterConstructor(
+    { initialValue, counterClass }: Props<CounterProps>,
+    refs: CounterElementRefs,
+) {
     const [count, setCount] = createSignal(initialValue);
 
     refs.subtracter.onclick(() => setCount(count() - 1));
     refs.adderButton.onclick(() => setCount(count() + 1));
 
+    const onChange = createEvent<CounterEvent>((emitter) => {
+        emitter.emit({ value: count() });
+    });
+
     return {
         render: () => ({ count, counterClass }),
+        onChange,
     };
 }
 
--- examples/jay/large-number-of-components/src/host.ts
+++ examples/jay/large-number-of-components/src/host.ts
@@ -1,44 +1,73 @@
-import {render, HostElementRefs, Counter} from './host.jay-html';
-import {createDerivedArray, createMemo, createSignal, makeJayComponent, Props} from 'jay-component';
+import { Counter, HostElementRefs, render } from './host.jay-html';
+import {
+    createDerivedArray,
+    createEffect,
+    createMemo,
+    createSignal,
+    makeJayComponent,
+    Props,
+} from 'jay-component';
+import { patch, REPLACE } from 'jay-json-patch';
+import { JayEvent } from 'jay-runtime';
+import { CounterEvent } from './counter';
 
-export interface HostProps {
-}
+export interface HostProps {}
 
 const COUNTER_CLASSES = [
-    "blueCounter", "lavenderCounter", "grayCounter", "purpleCounter", "yellowCounter"]
+    'blueCounter',
+    'lavenderCounter',
+    'grayCounter',
+    'purpleCounter',
+    'yellowCounter',
+];
 
 let nextCreationIndex = 0;
-interface InternalCounter {
+interface CounterData {
     currentCount: number;
     key: string;
 }
-function HostConstructor({  }: Props<HostProps>, refs: HostElementRefs) {
-
+function HostConstructor({}: Props<HostProps>, refs: HostElementRefs) {
     const [numberOfCounters, setNumberOfCounters] = createSignal(100);
-    const internalCounters = createMemo<InternalCounter[]>(prev => {
-        for (let i = prev.length; i < numberOfCounters(); i++)
-            prev.push({currentCount: i, key: ''+nextCreationIndex++})
-        return prev.slice(0, numberOfCounters());
-    }, [])
+    const [counterData, setCounterData] = createSignal<CounterData[]>([]);
 
-    const counters = createDerivedArray<InternalCounter, Counter>(internalCounters,
-        (internalCounter) => {
-        return ({
+    createEffect(() => {
+        const prev = counterData();
+        if (prev.length !== numberOfCounters()) {
+            for (let i = prev.length; i < numberOfCounters(); i++)
+                prev.push({ currentCount: i, key: '' + nextCreationIndex++ });
+            setCounterData(prev.slice(0, numberOfCounters()));
+        }
+    });
+
+    const counters = createDerivedArray<CounterData, Counter>(counterData, (internalCounter) => {
+        return {
             key: internalCounter().key,
             count: internalCounter().currentCount,
-            counterClass: COUNTER_CLASSES[internalCounter().currentCount % COUNTER_CLASSES.length]
-        })
-    })
+            counterClass: COUNTER_CLASSES[internalCounter().currentCount % COUNTER_CLASSES.length],
+        };
+    });
 
     const total = createMemo(() => {
-        return internalCounters().reduce((prev, curr) => prev + curr.currentCount, 0)
-    })
+        return counterData().reduce((prev, curr) => prev + curr.currentCount, 0);
+    });
 
-    refs.numberOfCounters.oninput(({event}) => {
+    refs.numberOfCounters.oninput(({ event }) => {
         const newValue = (event.target as HTMLInputElement).value;
-        if (Number(newValue))
-            setNumberOfCounters(Number(newValue))
-    })
+        if (Number(newValue)) setNumberOfCounters(Number(newValue));
+    });
+
+    refs.counter.onChange(({ coordinate, event }: JayEvent<CounterEvent, Counter>) => {
+        console.log(coordinate, event);
+        const counterIndex = counterData().findIndex(
+            (internalCounter) => internalCounter.key === coordinate[0],
+        );
+        if (counterData()[counterIndex].currentCount !== event.value)
+            setCounterData(
+                patch(counterData(), [
+                    { op: REPLACE, path: [counterIndex, 'currentCount'], value: event.value },
+                ]),
+            );
+    });
 
     return {
         render: () => ({ numberOfCounters, counters, total }),
