003764938777f75ebc96f36452bb05df0a1cb479
yoav <yoavaa@gmail.com>
2025-01-16 13:39:11 +0200

    testing events on svg

--- packages/runtime/runtime/test/lib/element-ns.test.ts
+++ packages/runtime/runtime/test/lib/element-ns.test.ts
@@ -1,5 +1,6 @@
 import {ConstructContext, dynamicAttribute as da, element as e, ReferencesManager, svgElement as svg} from '../../lib';
-const { JSDOM } = require('jsdom');
+
+const refName = 'ref1'
 
 describe('elements with namespace', () => {
     describe('svg', () => {
@@ -8,10 +9,10 @@ describe('elements with namespace', () => {
             color: string
         }
         function mkSvgElement(vs: ViewState) {
-            const [refManager, []] = ReferencesManager.for({}, [], [], [], []);
+            const [refManager, [ref1]] = ReferencesManager.for({}, [refName], [], [], []);
             const jayElement = ConstructContext.withRootContext(vs, refManager, () =>
                 svg('svg', { width: '200', height: '200', viewBox: '0 0 200' }, [
-                    svg('circle', { cx: '100', cy: '100', r: '50', fill: da(vs => vs.color) }, []),
+                    svg('circle', { cx: '100', cy: '100', r: '50', fill: da(vs => vs.color) }, [], ref1()),
                 ]),
             );
             return {jayElement}
@@ -28,6 +29,17 @@ describe('elements with namespace', () => {
             jayElement.update({color: 'red'})
 
             expect(jayElement.dom.outerHTML).toBe('<svg width="200" height="200" viewBox="0 0 200"><circle cx="100" cy="100" r="50" fill="red"></circle></svg>');
+        });
+
+        it('should support events on svg', () => {
+            const fn = vitest.fn();
+            const {jayElement} = mkSvgElement({color: 'blue'})
+            jayElement.refs[refName].onclick(fn);
+
+            (jayElement.dom as SVGElement).getElementsByTagName('circle')[0]
+                .dispatchEvent(new window.Event('click'));
+
+            expect(fn).toHaveBeenCalled()
         });
     });
 });
