89bdacd2821ee653fceb5709c6cbd20f5bd5cd86
yoav <yoavaa@gmail.com>
2025-01-16 13:26:01 +0200

    dynamic svg attribute test

--- packages/runtime/runtime/test/lib/element-ns.test.ts
+++ packages/runtime/runtime/test/lib/element-ns.test.ts
@@ -1,14 +1,33 @@
-import { svgElement as svg } from '../../lib';
+import {ConstructContext, dynamicAttribute as da, element as e, ReferencesManager, svgElement as svg} from '../../lib';
 const { JSDOM } = require('jsdom');
 
 describe('elements with namespace', () => {
     describe('svg', () => {
+
+        interface ViewState {
+            color: string
+        }
+        function mkSvgElement(vs: ViewState) {
+            const [refManager, []] = ReferencesManager.for({}, [], [], [], []);
+            const jayElement = ConstructContext.withRootContext(vs, refManager, () =>
+                svg('svg', { width: '200', height: '200', viewBox: '0 0 200' }, [
+                    svg('circle', { cx: '100', cy: '100', r: '50', fill: da(vs => vs.color) }, []),
+                ]),
+            );
+            return {jayElement}
+        }
+
         it('should create svg element', () => {
-            const jayElement = svg('svg', { width: '200', height: '200', viewBox: '0 0 200' }, [
-                svg('circle', { cx: '100', cy: '100', r: '50', fill: 'blue' }, []),
-            ]);
+            const {jayElement} = mkSvgElement({color: 'blue'})
 
             expect(jayElement.dom.outerHTML).toBe('<svg width="200" height="200" viewBox="0 0 200"><circle cx="100" cy="100" r="50" fill="blue"></circle></svg>');
+        });
+
+        it('should support dynamic attribute', () => {
+            const {jayElement} = mkSvgElement({color: 'blue'})
+            jayElement.update({color: 'red'})
+
+            expect(jayElement.dom.outerHTML).toBe('<svg width="200" height="200" viewBox="0 0 200"><circle cx="100" cy="100" r="50" fill="red"></circle></svg>');
         });
     });
 });
