f9c499eaf68d38e344a1baa22a327d7486bda678
yoav <yoavaa@gmail.com>
2025-03-16 21:53:25 +0200

    more cleanup

--- examples/jay-stack/fake-shop/src/entry-client.ts
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import './typescript.svg'
-import { setupCounter } from './counter'
-
-setupCounter(document.querySelector('#counter') as HTMLButtonElement)
-
-export function init() {
-    console.log('init')
-}
