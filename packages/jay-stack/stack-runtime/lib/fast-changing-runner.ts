73d9ecb665b2e15f11c61beecc91ad2f324a4cb0
yoav <yoavaa@gmail.com>
2025-03-16 18:20:32 +0200

    added default handling for optional fast rendering

--- packages/jay-stack/stack-runtime/lib/fast-changing-runner.ts
+++ packages/jay-stack/stack-runtime/lib/fast-changing-runner.ts
@@ -1,9 +1,13 @@
 import {AnyFastRenderResult, AnyJayStackComponentDefinition, PageProps} from "./jay-stack-types";
+import {partialRender} from "./render-results";
 
 export function renderFastChangingData(componentDefinition: AnyJayStackComponentDefinition,
                                        pageParams: object,
                                        pageProps: PageProps,
                                        carryForward: object):
     Promise<AnyFastRenderResult> {
-    return componentDefinition.fastRender({...pageParams, ...pageProps, ...carryForward});
+    if (componentDefinition.fastRender)
+        return componentDefinition.fastRender({...pageParams, ...pageProps, ...carryForward});
+    else
+        return Promise.resolve(partialRender({}, {}));
 }
