ad59646ae1d12443b127fa223197e82a853a1222
yoav <yoavaa@gmail.com>
2025-04-06 17:34:42 +0300

    stores plugin fixture

---
+++ packages/jay-stack/stack-runtime/test/fixtures/stores-plugin/discount.jay-contract.d.ts
@@ -0,0 +1,22 @@
+import {JayElement, RenderElement, RenderElementOptions} from "jay-runtime";
+
+
+export enum Type {
+  amount,
+  percent
+}
+
+export interface DiscountViewState {
+  type: Type,
+  value: number
+}
+
+export interface DiscountRefs {}
+
+export interface DiscountRepeatedRefs {}
+
+export type DiscountElement = JayElement<DiscountViewState, DiscountRefs>
+export type DiscountElementRender = RenderElement<DiscountViewState, DiscountRefs, DiscountElement>
+export type DiscountElementPreRender = [DiscountRefs, DiscountElementRender]
+
+export declare function render(options?: RenderElementOptions): DiscountElementPreRender
---
+++ packages/jay-stack/stack-runtime/test/fixtures/stores-plugin/product-page.jay-contract.d.ts
@@ -0,0 +1,62 @@
+import {JayElement, RenderElement, HTMLElementCollectionProxy, HTMLElementProxy, RenderElementOptions} from "jay-runtime";
+import {DiscountViewState, DiscountRefs, DiscountRepeatedRefs} from "./discount.jay-contract";
+import {MediaItemViewState, MediaItemRefs, MediaItemRepeatedRefs} from "./media-item.jay-contract";
+
+export interface Media {
+  items: Array<MediaItemViewState>,
+  mainMedia: MediaItemViewState
+}
+
+export interface Formatted {
+  discountedPrice: string,
+  price: string,
+  pricePerUnit: string
+}
+
+export interface PriceData {
+  currency: string,
+  discountedPrice: number,
+  formatted: Formatted,
+  price: number,
+  pricePerUnit: number
+}
+
+export enum ProductType {
+  digital,
+  physical
+}
+
+export interface ProductPageViewState {
+  id: string,
+  brand: string,
+  description: string,
+  discount: DiscountViewState,
+  media: Media,
+  name: string,
+  inventoryItemId: string,
+  priceData: PriceData,
+  productType: ProductType,
+  ribbon: string
+}
+
+
+export interface ProductPageRefs {
+  discount: DiscountRefs,
+  items: MediaItemRepeatedRefs,
+  mainMedia: MediaItemRefs,
+  addToCart: HTMLElementProxy<ProductPageViewState, HTMLButtonElement>
+}
+
+
+export interface ProductPageRepeatedRefs {
+  discount: DiscountRepeatedRefs,
+  items: MediaItemRepeatedRefs,
+  mainMedia: MediaItemRepeatedRefs,
+  addToCart: HTMLElementCollectionProxy<ProductPageViewState, HTMLButtonElement>
+}
+
+export type ProductPageElement = JayElement<ProductPageViewState, ProductPageRefs>
+export type ProductPageElementRender = RenderElement<ProductPageViewState, ProductPageRefs, ProductPageElement>
+export type ProductPageElementPreRender = [ProductPageRefs, ProductPageElementRender]
+
+export declare function render(options?: RenderElementOptions): ProductPageElementPreRender
