856f7dc69520de1de3bf338a35d1f70aafa530e8
yoav <yoavaa@gmail.com>
2025-04-07 15:54:24 +0300

    fixed the file scanning for generating jay-html to ts files

--- packages/compiler/cli/lib/find-all-jay-files.ts
+++ packages/compiler/cli/lib/find-all-jay-files.ts
@@ -2,5 +2,5 @@ import { glob } from 'fast-glob';
 import { JAY_EXTENSION } from 'jay-compiler-shared';
 
 export async function findAllJayFiles(dir): Promise<string[]> {
-    return await glob(`${dir}/**/*{${JAY_EXTENSION}}`);
+    return await glob(`${dir}/**/*${JAY_EXTENSION}`);
 }
