42aaa7e4751949ce0b1332ac18e0e00505f6aca8
yoav <yoavaa@gmail.com>
2025-04-09 16:26:40 +0300

    created simple plugin

---
+++ packages/jay-stack/stack-runtime/test/simple-plugin/simple-plugin.ts
@@ -0,0 +1,61 @@
+import { makeJayStackComponent, PageProps, partialRender } from 'jay-stack-runtime';
+import { render, SimplePluginViewState, SimplePluginRefs } from './compiled/simple-plugin.jay-contract';
+import { createSignal, Props } from 'jay-component';
+import { PartialRender } from "../../lib";
+
+// Define view states
+type StaticViewState = Pick<SimplePluginViewState, "pluginSlowlyRendered">
+type DynamicViewState = Omit<SimplePluginViewState, keyof StaticViewState>
+
+// Define carry forward interfaces
+interface StaticCarryForward {
+    staticData: string;
+}
+
+interface DynamicCarryForward {
+    dynamicData: string;
+    pluginInteractiveRendered: string;
+}
+
+// Static rendering function
+async function renderStaticContent(props: PageProps): Promise<PartialRender<StaticViewState, StaticCarryForward>> {
+    const pluginSlowlyRendered = 'This is static content from a plugin';
+    const staticData = 'Static plugin data to carry forward';
+    return partialRender({ pluginSlowlyRendered }, { staticData });
+}
+
+// Dynamic rendering function
+async function renderDynamicContent(props: PageProps & StaticCarryForward): Promise<PartialRender<DynamicViewState, DynamicCarryForward>> {
+    const pluginInteractiveRendered = `Dynamic content from plugin using carry forward: ${props.staticData}`;
+    const dynamicData = 'Dynamic data to carry forward';
+    return partialRender(
+        { pluginInteractiveRendered },
+        { dynamicData, pluginInteractiveRendered }
+    );
+}
+
+// Interactive component constructor
+function SimplePluginConstructor(
+    props: Props<PageProps & DynamicCarryForward>,
+    refs: SimplePluginRefs,
+) {
+    const [dynamicContent, setDynamicContent] = createSignal(props.pluginInteractiveRendered);
+
+    refs.pluginButton.onclick(() => {
+        setDynamicContent(
+            `Updated dynamic content using dynamic data: ${props.dynamicData()}`
+        );
+    });
+
+    return {
+        render: () => ({
+            pluginInteractiveRendered: dynamicContent,
+        }),
+    };
+}
+
+export const plugin = makeJayStackComponent(render)
+    .withProps<PageProps>()
+    .withSlowlyRender<StaticViewState, StaticCarryForward>(renderStaticContent)
+    .withFastRender<DynamicCarryForward>(renderDynamicContent)
+    .withInteractive(SimplePluginConstructor); 
