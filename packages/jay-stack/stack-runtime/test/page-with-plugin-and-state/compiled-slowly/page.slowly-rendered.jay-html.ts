eda9b1bf284a43eea4be39e569ec0b8aea50fe3b
yoav <yoavaa@gmail.com>
2025-04-21 12:23:20 +0300

    added a test for both plugin and a page

---
+++ packages/jay-stack/stack-runtime/test/page-with-plugin-and-state/compiled-slowly/page.slowly-rendered.jay-html.ts
@@ -0,0 +1,52 @@
+import {
+    JayElement,
+    element as e,
+    dynamicText as dt,
+    RenderElement,
+    ReferencesManager,
+    ConstructContext,
+    HTMLElementProxy,
+    RenderElementOptions,
+} from 'jay-runtime';
+import {SimplePluginRefs, SimplePluginViewState} from "../../simple-plugin/compiled/simple-plugin.jay-contract";
+
+export interface PageViewState {
+    plugin: SimplePluginViewState,
+    pageSlowlyRendered: string,
+    pageFastDynamicRendered: string
+}
+
+export interface PageElementRefs {
+    plugin: SimplePluginRefs,
+    button: HTMLElementProxy<PageViewState, HTMLButtonElement>
+}
+
+export type PageElement = JayElement<PageViewState, PageElementRefs>;
+export type PageElementRender = RenderElement<PageViewState, PageElementRefs, PageElement>;
+export type PageElementPreRender = [PageElementRefs, PageElementRender];
+
+export function render(options?: RenderElementOptions): PageElementPreRender {
+    const [pluginRefManager, [refPluginButton]] =
+        ReferencesManager.for(options, ['pluginButton'], [], [], []);
+    const [refManager, [refButton]] =
+        ReferencesManager.for(options, ['button'], [], [], [], {plugin: pluginRefManager});
+    
+    const render = (viewState: PageViewState) =>
+        ConstructContext.withRootContext(viewState, refManager, () =>
+            e('div', {}, [
+                // Plugin values section
+                e('h2', {}, ['Plugin Values']),
+                e('div', {}, ['Plugin Slow: SLOWLY RENDERED']),
+                e('div', {}, [dt(vs => `Plugin Fast: ${vs.plugin.pluginInteractiveRendered}`)]),
+                e('button', { 'data-id': 'plugin-button' }, ['Plugin Button'], refPluginButton()),
+                
+                // Page values section
+                e('h2', {}, ['Page Values']),
+                e('div', {}, ['Page Slow: SLOWLY RENDERED']),
+                e('div', {}, [dt(vs => `Page Fast: ${vs.pageFastDynamicRendered}`)]),
+                e('button', { 'data-id': 'page-button' }, ['Page Button'], refButton())
+            ]),
+        ) as PageElement;
+    
+    return [refManager.getPublicAPI() as PageElementRefs, render];
+} 
---
+++ packages/jay-stack/stack-runtime/test/page-with-plugin-and-state/page-with-plugin-and-state.test.ts
@@ -0,0 +1,178 @@
+import {
+    DevSlowlyChangingPhase,
+    makeCompositeJayComponent,
+    PageProps,
+    partialRender,
+    renderFastChangingData
+} from '../../lib';
+import { render } from './compiled-slowly/page.slowly-rendered.jay-html'
+import { prettify } from 'jay-compiler-shared';
+import { plugin } from '../simple-plugin/simple-plugin'
+import { page } from './page'
+
+const PAGE_PROPS: PageProps = {
+    language: 'en-us',
+};
+const PAGE_PARAMS = {};
+const PAGE_PARTS = [
+    {compDefinition: plugin, key: 'plugin'},
+    {compDefinition: page}
+];
+
+describe('rendering a page with plugin and state', () => {
+    it('should run the slowly changing phase', async () => {
+        const slowlyPhase = new DevSlowlyChangingPhase();
+
+        const slowlyRenderResult = await slowlyPhase.runSlowlyForPage(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            PAGE_PARTS
+        );
+
+        expect(slowlyRenderResult).toEqual(
+            partialRender(
+                {
+                    plugin: {
+                        pluginSlowlyRendered: "SLOWLY RENDERED",
+                    },
+                    pageSlowlyRendered: "SLOWLY RENDERED",
+                },
+                {
+                    plugin: {
+                        staticData: "SLOWLY -> FAST CARRY FORWARD",
+                    },
+                    carryForwardSlowly: "SLOWLY -> FAST CARRY FORWARD",
+                },
+            ),
+        );
+    });
+
+    it('should run the fast changing phase, getting the carry forward from the slowly phase', async () => {
+        const slowlyPhase = new DevSlowlyChangingPhase();
+        const slowlyRenderResult = await slowlyPhase.runSlowlyForPage(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            PAGE_PARTS
+        );
+        if (slowlyRenderResult.kind !== 'PartialRender')
+            throw new Error('expecting partial render from slowly phase');
+
+        const fastRenderResult = await renderFastChangingData(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            slowlyRenderResult.carryForward,
+            PAGE_PARTS
+        );
+
+        expect(fastRenderResult).toEqual(
+            partialRender(
+                {
+                    plugin: {
+                        pluginInteractiveRendered: "FAST RENDERED, using SLOWLY -> FAST CARRY FORWARD",
+                    },
+                    pageFastDynamicRendered: "FAST RENDERED, using 'SLOWLY -> FAST CARRY FORWARD'",
+                },
+                {
+                    plugin: {
+                        dynamicData: "FAST -> INTERACTIVE CARRY FORWARD",
+                        pluginInteractiveRendered: "FAST RENDERED, using SLOWLY -> FAST CARRY FORWARD",
+                    },
+                    carryForwardFast: "FAST -> INTERACTIVE CARRY FORWARD",
+                    pageFastDynamicRendered: "FAST RENDERED, using 'SLOWLY -> FAST CARRY FORWARD'",
+                },
+            ),
+        );
+    });
+
+    it('should run the interactive phase, getting the carry forward from the fast phase', async () => {
+        const slowlyPhase = new DevSlowlyChangingPhase();
+        const slowlyRenderResult = await slowlyPhase.runSlowlyForPage(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            PAGE_PARTS
+        );
+        if (slowlyRenderResult.kind !== 'PartialRender')
+            throw new Error('expecting partial render from slowly phase');
+        const fastRenderResult = await renderFastChangingData(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            slowlyRenderResult.carryForward,
+            PAGE_PARTS
+        );
+        if (fastRenderResult.kind !== 'PartialRender')
+            throw new Error('expecting partial render from fast phase');
+        const fastCarryForward = fastRenderResult.carryForward;
+
+        const comp = makeCompositeJayComponent(render, fastRenderResult.rendered, fastCarryForward, PAGE_PARTS)
+        const instance = comp({ ...PAGE_PROPS } as any);
+
+        expect(await prettify(instance.element.dom.outerHTML)).toEqual(
+            await prettify(`
+            <div>
+                <h2>Plugin Values</h2>
+                <div>Plugin Slow: SLOWLY RENDERED</div>
+                <div>Plugin Fast: FAST RENDERED, using SLOWLY -&gt; FAST CARRY FORWARD</div>
+                <button data-id="plugin-button">Plugin Button</button>
+                <h2>Page Values</h2>
+                <div>Page Slow: SLOWLY RENDERED</div>
+                <div>Page Fast: FAST RENDERED, using 'SLOWLY -&gt; FAST CARRY FORWARD'</div>
+                <button data-id="page-button">Page Button</button>
+            </div>`),
+        );
+    });
+
+    it('interactive phase should function and react to events for both plugin and page', async () => {
+        const slowlyPhase = new DevSlowlyChangingPhase();
+        const slowlyRenderResult = await slowlyPhase.runSlowlyForPage(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            PAGE_PARTS
+        );
+        if (slowlyRenderResult.kind !== 'PartialRender')
+            throw new Error('expecting partial render from slowly phase');
+        const fastRenderResult = await renderFastChangingData(
+            PAGE_PARAMS,
+            PAGE_PROPS,
+            slowlyRenderResult.carryForward,
+            PAGE_PARTS
+        );
+        if (fastRenderResult.kind !== 'PartialRender')
+            throw new Error('expecting partial render from fast phase');
+        const fastCarryForward = fastRenderResult.carryForward;
+
+        const comp = makeCompositeJayComponent(render, fastRenderResult.rendered, fastCarryForward, PAGE_PARTS)
+        const instance = comp({ ...PAGE_PROPS } as any);
+
+        // Test plugin button click
+        await instance.element.refs.plugin.pluginButton.exec$((_) => _.click());
+        expect(await prettify(instance.element.dom.outerHTML)).toEqual(
+            await prettify(`
+            <div>
+                <h2>Plugin Values</h2>
+                <div>Plugin Slow: SLOWLY RENDERED</div>
+                <div>Plugin Fast: INTERACTIVE RENDERED, using FAST -&gt; INTERACTIVE CARRY FORWARD</div>
+                <button data-id="plugin-button">Plugin Button</button>
+                <h2>Page Values</h2>
+                <div>Page Slow: SLOWLY RENDERED</div>
+                <div>Page Fast: FAST RENDERED, using 'SLOWLY -&gt; FAST CARRY FORWARD'</div>
+                <button data-id="page-button">Page Button</button>
+            </div>`),
+        );
+
+        // Test page button click
+        await instance.element.refs.button.exec$((_) => _.click());
+        expect(await prettify(instance.element.dom.outerHTML)).toEqual(
+            await prettify(`
+            <div>
+                <h2>Plugin Values</h2>
+                <div>Plugin Slow: SLOWLY RENDERED</div>
+                <div>Plugin Fast: INTERACTIVE RENDERED, using FAST -&gt; INTERACTIVE CARRY FORWARD</div>
+                <button data-id="plugin-button">Plugin Button</button>
+                <h2>Page Values</h2>
+                <div>Page Slow: SLOWLY RENDERED</div>
+                <div>Page Fast: INTERACTIVE RENDERED, using 'FAST -&gt; INTERACTIVE CARRY FORWARD'</div>
+                <button data-id="page-button">Page Button</button>
+            </div>`),
+        );
+    });
+}); 
