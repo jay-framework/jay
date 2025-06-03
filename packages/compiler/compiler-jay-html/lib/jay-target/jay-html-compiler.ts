7e11e8b457cb3675ac593074c744d1b6d4cf204e
noamsi <noamsi@wix.com>
2025-06-03 16:46:35 +0300

    handle background with url style value

--- packages/compiler/compiler-jay-html/lib/jay-target/jay-html-compiler.ts
+++ packages/compiler/compiler-jay-html/lib/jay-target/jay-html-compiler.ts
@@ -90,7 +90,7 @@ function renderAttributes(element: HTMLElement, { variables }: RenderContext): R
             return;
         if (attrCanonical === 'style')
             renderedAttributes.push(
-                new RenderFragment(`style: {cssText: '${attributes[attrName]}'}`),
+                new RenderFragment(`style: {cssText: '${attributes[attrName].replace(/'/g, "\\'")}'}`),
             );
         else if (attrCanonical === 'class') {
             let classExpression = parseClassExpression(attributes[attrName], variables);
