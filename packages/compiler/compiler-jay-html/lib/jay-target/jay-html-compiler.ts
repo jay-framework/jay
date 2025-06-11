1031e2e7cfcff887e0178d3fd0abbbb38fcd4a8d
noamsi <noamsi@wix.com>
2025-06-11 18:40:51 +0300

    Cosmetics

--- packages/compiler/compiler-jay-html/lib/jay-target/jay-html-compiler.ts
+++ packages/compiler/compiler-jay-html/lib/jay-target/jay-html-compiler.ts
@@ -513,7 +513,8 @@ ${Indent.forceIndent(code, 4)},
     const body = `export function render(options?: RenderElementOptions): ${preRenderType} {
     const [refManager, [${refVariables}]] =
         ReferencesManager.for(options, [${elemRefsDeclarations}], [${elemCollectionRefsDeclarations}], [${compRefsDeclarations}], [${compCollectionRefsDeclarations}]);
-    ${headLinksInjection}const render = (viewState: ${viewStateType}) => ConstructContext.withRootContext(
+    ${headLinksInjection}
+    const render = (viewState: ${viewStateType}) => ConstructContext.withRootContext(
         viewState, refManager,
         () => ${renderedRoot.rendered.trim()}
     ) as ${elementType};
