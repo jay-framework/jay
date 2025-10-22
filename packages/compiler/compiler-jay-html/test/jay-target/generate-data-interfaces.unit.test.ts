bf6eeacca012a2a03e8a9b0b7e7014e828e51543
yoav <yoavaa@gmail.com>
2025-10-22 10:01:28 +0300

    generating recursive interface types

--- packages/compiler/compiler-jay-html/test/jay-target/generate-data-interfaces.unit.test.ts
+++ packages/compiler/compiler-jay-html/test/jay-target/generate-data-interfaces.unit.test.ts
@@ -1,18 +1,18 @@
-import stripMargin from '@caiogondim/strip-margin';
-
 import {
     JayArrayType,
     JayBoolean,
     JayDate,
     JayNumber,
     JayObjectType,
+    JayRecursiveType,
     JayString,
+    prettify,
 } from '@jay-framework/compiler-shared';
 
 import { generateTypes } from '../../lib';
 
 describe('generate data interfaces', () => {
-    it('should generate simple interface', () => {
+    it('should generate simple interface', async () => {
         let genInterface = generateTypes(
             new JayObjectType('ElementNameViewState', {
                 name: JayString,
@@ -21,19 +21,17 @@ describe('generate data interfaces', () => {
                 bdate: JayDate,
             }),
         );
-        expect(genInterface).toEqual(
-            stripMargin(
-                `export interface ElementNameViewState {
-                |  name: string,
-                |  age: number,
-                |  bool: boolean,
-                |  bdate: Date
-                |}`,
-            ),
+        expect(await prettify(genInterface)).toEqual(
+            await prettify(`export interface ElementNameViewState {
+  name: string,
+  age: number,
+  bool: boolean,
+  bdate: Date
+}`),
         );
     });
 
-    it('should generate interface with complex object types', () => {
+    it('should generate interface with complex object types', async () => {
         let genInterface = generateTypes(
             new JayObjectType('ElementNameViewState', {
                 name: JayString,
@@ -42,21 +40,19 @@ describe('generate data interfaces', () => {
                 }),
             }),
         );
-        expect(genInterface).toEqual(
-            stripMargin(
-                `export interface Address {
-                |  street: string
-                |}
-                |
-                |export interface ElementNameViewState {
-                |  name: string,
-                |  address: Address
-                |}`,
-            ),
+        expect(await prettify(genInterface)).toEqual(
+            await prettify(`export interface Address {
+  street: string
+}
+
+export interface ElementNameViewState {
+  name: string,
+  address: Address
+}`),
         );
     });
 
-    it('should generate interface with complex array of object types', () => {
+    it('should generate interface with complex array of object types', async () => {
         let genInterface = generateTypes(
             new JayObjectType('ElementNameViewState', {
                 name: JayString,
@@ -67,17 +63,120 @@ describe('generate data interfaces', () => {
                 ),
             }),
         );
-        expect(genInterface).toEqual(
-            stripMargin(
-                `export interface Address {
-                |  street: string
-                |}
-                |
-                |export interface ElementNameViewState {
-                |  name: string,
-                |  address: Array<Address>
-                |}`,
-            ),
+        expect(await prettify(genInterface)).toEqual(
+            await prettify(`export interface Address {
+  street: string
+}
+
+export interface ElementNameViewState {
+  name: string,
+  address: Array<Address>
+}`),
         );
+    });
+
+    describe('recursive types', () => {
+        it('should generate recursive interface with array recursion', async () => {
+            const rootType = new JayObjectType('TreeViewState', {
+                name: JayString,
+                id: JayString,
+                children: new JayArrayType(new JayRecursiveType('$/data')),
+            });
+            
+            // Resolve the recursive reference
+            const childrenArray = rootType.props.children as JayArrayType;
+            const recursiveType = childrenArray.itemType as JayRecursiveType;
+            recursiveType.resolvedType = rootType;
+            
+            let genInterface = generateTypes(rootType);
+            
+            expect(await prettify(genInterface)).toEqual(
+                await prettify(`export interface TreeViewState {
+  name: string,
+  id: string,
+  children: Array<TreeViewState>
+}`),
+            );
+        });
+
+        it('should generate recursive interface with single optional child', async () => {
+            const rootType = new JayObjectType('LinkedListViewState', {
+                value: JayString,
+                id: JayString,
+                next: new JayRecursiveType('$/data'),
+            });
+            
+            // Resolve the recursive reference
+            const recursiveType = rootType.props.next as JayRecursiveType;
+            recursiveType.resolvedType = rootType;
+            
+            let genInterface = generateTypes(rootType);
+            
+            expect(await prettify(genInterface)).toEqual(
+                await prettify(`export interface LinkedListViewState {
+  value: string,
+  id: string,
+  next: LinkedListViewState | null
+}`),
+            );
+        });
+
+        it('should generate recursive interface with indirect recursion', async () => {
+            const rootType = new JayObjectType('MenuViewState', {
+                name: JayString,
+                id: JayString,
+                submenu: new JayObjectType('SubmenuOfMenuViewState', {
+                    title: JayString,
+                    items: new JayArrayType(new JayRecursiveType('$/data')),
+                }),
+            });
+            
+            // Resolve the recursive reference
+            const submenu = rootType.props.submenu as JayObjectType;
+            const items = submenu.props.items as JayArrayType;
+            const recursiveType = items.itemType as JayRecursiveType;
+            recursiveType.resolvedType = rootType;
+            
+            let genInterface = generateTypes(rootType);
+            
+            expect(await prettify(genInterface)).toEqual(
+                await prettify(`export interface SubmenuOfMenuViewState {
+  title: string,
+  items: Array<MenuViewState>
+}
+
+export interface MenuViewState {
+  name: string,
+  id: string,
+  submenu: SubmenuOfMenuViewState
+}`),
+            );
+        });
+
+        it('should generate recursive interface with multiple optional children', async () => {
+            const rootType = new JayObjectType('BinaryTreeViewState', {
+                value: JayNumber,
+                id: JayString,
+                left: new JayRecursiveType('$/data'),
+                right: new JayRecursiveType('$/data'),
+            });
+            
+            // Resolve the recursive references
+            const leftRecursive = rootType.props.left as JayRecursiveType;
+            leftRecursive.resolvedType = rootType;
+            const rightRecursive = rootType.props.right as JayRecursiveType;
+            rightRecursive.resolvedType = rootType;
+            
+            let genInterface = generateTypes(rootType);
+            
+            expect(await prettify(genInterface)).toEqual(
+                await prettify(`export interface BinaryTreeViewState {
+  value: number,
+  id: string,
+  left: BinaryTreeViewState | null,
+  right: BinaryTreeViewState | null
+}`),
+            );
+        });
     });
 });
