4c64d8a6f9a99cf30bd7817f20b04ba1b5f3ebf6
yoav <yoavaa@gmail.com>
2025-11-23 20:14:54 +0200

    reverted all the work on type mapping, and updated the design log

--- packages/jay-stack/full-stack-component/lib/index.ts
+++ packages/jay-stack/full-stack-component/lib/index.ts
@@ -1,5 +1,3 @@
 export * from './render-results';
 export * from './jay-stack-types';
 export * from './jay-stack-builder';
-export * from './rendering-manifest';
-export * from './manifest-validator';
