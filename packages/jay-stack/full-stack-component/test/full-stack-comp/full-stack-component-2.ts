76b27877cc7659c24c9f4bf0edacb61dbc2b5dae
yoav <yoavaa@gmail.com>
2025-11-24 23:52:56 +0200

    added another example of full stack component without inline functions

---
+++ packages/jay-stack/full-stack-component/test/full-stack-comp/full-stack-component-2.ts
@@ -0,0 +1,53 @@
+import {
+    FSComponentContract, FSComponentFastViewState, FSComponentSlowViewState,
+} from './full-stack-component.jay-html';
+import {
+    makeJayStackComponent,
+    partialRender,
+    createJayService, RenderSlowly, SlowlyRenderResult, FastRenderResult,
+} from '../../lib';
+
+interface FSCProps {}
+
+interface MyContext {}
+const MyContextMarker = createJayService<MyContext>();
+
+async function slowlyRender(props, myContext): Promise<SlowlyRenderResult<FSComponentSlowViewState, {id: string}>> {
+    return partialRender(
+        {
+            id: '1',
+            name: 'Joe',
+            age: 32,
+            address: '25 W 14 st, NY, NY',
+        },
+        {id: '1'},
+    );
+}
+
+
+async function fastRender(props, myContext): Promise<FastRenderResult<FSComponentFastViewState, {id: string }>>{
+    return partialRender(
+        {
+            stars: 12,
+            rating: 13,
+        },
+        {id: '1'},
+    );
+}
+
+
+function interactive(props, refs) {
+    return {
+        render: () => ({
+            stars: 14,
+            rating: 15,
+        }),
+    };
+}
+
+export const comp = makeJayStackComponent<FSComponentContract>()
+    .withProps<FSCProps>()
+    .withServices(MyContextMarker)
+    .withSlowlyRender(slowlyRender)
+    .withFastRender(fastRender)
+    .withInteractive(interactive);
