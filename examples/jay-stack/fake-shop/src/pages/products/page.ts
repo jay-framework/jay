cfbadc566c6cedb3893fd4668de7d82705f66bbb
yoav <yoavaa@gmail.com>
2025-12-04 21:10:29 +0200

    update examples with RenderPipeline

--- examples/jay-stack/fake-shop/src/pages/products/page.ts
+++ examples/jay-stack/fake-shop/src/pages/products/page.ts
@@ -1,24 +1,40 @@
 import {
     makeJayStackComponent,
     PageProps,
-    partialRender,
+    RenderPipeline,
 } from '@jay-framework/fullstack-component';
-import { PageContract, PageElementRefs, ProductOfPageViewState } from './page.jay-html';
+import {
+    PageContract,
+    PageElementRefs,
+    PageSlowViewState,
+    PageFastViewState,
+} from './page.jay-html';
 import { Props } from '@jay-framework/component';
 import { PRODUCTS_DATABASE_SERVICE, ProductsDatabaseService } from '../../products-database';
 
 interface ProductsCarryForward {}
 
 async function renderSlowlyChanging(props: PageProps, productsDb: ProductsDatabaseService) {
-    const products = await productsDb.getProducts();
-    return partialRender({ products }, {});
+    const Pipeline = RenderPipeline.for<PageSlowViewState, ProductsCarryForward>();
+
+    return Pipeline.try(() => productsDb.getProducts())
+        .recover(() => Pipeline.serverError(503, 'Failed to load products'))
+        .toPhaseOutput((products) => ({
+            viewState: { products },
+            carryForward: {},
+        }));
 }
 
 async function renderFastChanging(
     props: PageProps & ProductsCarryForward,
     productsDb: ProductsDatabaseService,
 ) {
-    return partialRender({}, {});
+    const Pipeline = RenderPipeline.for<PageFastViewState, ProductsCarryForward>();
+
+    return Pipeline.ok({}).toPhaseOutput(() => ({
+        viewState: {},
+        carryForward: {},
+    }));
 }
 
 function ProductsPageConstructor(
