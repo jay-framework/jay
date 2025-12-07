5a536ce589817711d1a4027467700608e952066c
yoav <yoavaa@gmail.com>
2025-12-07 15:31:35 +0200

    simplified the approach for imported contract to jay-html, such that the jay-html.d.ts inlines all the types, potentially duplicate of contract.d.ts, but with inclusion of headless component types. The potential duplicate approach is simpler to support

--- packages/compiler/compiler-jay-html/test/fixtures/html-with-contract-ref/simple-with-contract/page.jay-html.d.ts
+++ packages/compiler/compiler-jay-html/test/fixtures/html-with-contract-ref/simple-with-contract/page.jay-html.d.ts
@@ -1,26 +1,36 @@
 import {
-    PageViewState,
-    PageRefs as PageElementRefs,
-    PageSlowViewState,
-    PageFastViewState,
-    PageInteractiveViewState,
-    PageContract,
-} from './page.jay-contract';
+    JayElement,
+    RenderElement,
+    HTMLElementProxy,
+    RenderElementOptions,
+    JayContract,
+} from '@jay-framework/runtime';
 
-import { JayElement, RenderElement, RenderElementOptions } from '@jay-framework/runtime';
+export interface PageViewState {
+    slowlyRendered: string;
+    fastRendered: string;
+    fastDynamicRendered: string;
+}
 
-// Re-export contract types for convenience
-export {
-    PageViewState,
-    PageElementRefs,
-    PageSlowViewState,
-    PageFastViewState,
-    PageInteractiveViewState,
-    PageContract,
-};
+export interface PageElementRefs {
+    button: HTMLElementProxy<PageViewState, HTMLButtonElement>;
+}
+
+export type PageSlowViewState = Pick<PageViewState, 'slowlyRendered'>;
+
+export type PageFastViewState = Pick<PageViewState, 'fastRendered' | 'fastDynamicRendered'>;
+
+export type PageInteractiveViewState = Pick<PageViewState, 'fastDynamicRendered'>;
 
 export type PageElement = JayElement<PageViewState, PageElementRefs>;
 export type PageElementRender = RenderElement<PageViewState, PageElementRefs, PageElement>;
 export type PageElementPreRender = [PageElementRefs, PageElementRender];
+export type PageContract = JayContract<
+    PageViewState,
+    PageElementRefs,
+    PageSlowViewState,
+    PageFastViewState,
+    PageInteractiveViewState
+>;
 
 export declare function render(options?: RenderElementOptions): PageElementPreRender;
