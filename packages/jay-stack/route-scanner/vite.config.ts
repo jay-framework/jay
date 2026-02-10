d759bbc1463db2674cd599ae9b48f509f66568ea
noamsi <noamsi@wix.com>
2026-02-10 10:03:50 +0200

    fix failing build route-scanner package change to ESM module

--- packages/jay-stack/route-scanner/vite.config.ts
+++ packages/jay-stack/route-scanner/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             entry: resolve(__dirname, 'lib/index.ts'),
             name: 'jayStackRuntime',
             fileName: 'index',
-            formats: ['cjs'],
+            formats: ['es'],
         },
         rollupOptions: {
             external: ['@jay-framework/logger'],
