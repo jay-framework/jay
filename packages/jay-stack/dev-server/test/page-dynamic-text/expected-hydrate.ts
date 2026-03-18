c584def822106aec236b4d682d0faa0ffc8c7c5f
yoav <yoavaa@gmail.com>
2026-03-18 02:01:48 +0200

    fixed test with change in handling isSlowPhase for slow render

--- packages/jay-stack/dev-server/test/page-dynamic-text/expected-hydrate.ts
+++ packages/jay-stack/dev-server/test/page-dynamic-text/expected-hydrate.ts
@@ -1,7 +1,6 @@
 import {
     ReferencesManager,
     ConstructContext,
-    adoptText,
     adoptElement,
 // @ts-ignore
 } from '/@fs/Users/yoav/work/jay/main/packages/runtime/runtime/dist/index.js';
@@ -9,10 +8,7 @@ export function hydrate(rootElement, options) {
     const [refManager, []] = ReferencesManager.for(options, [], [], [], []);
     const render = (viewState) =>
         ConstructContext.withHydrationRootContext(viewState, refManager, rootElement, () =>
-            adoptElement('0', {}, [
-                adoptText('0/0', (vs) => vs.title),
-                adoptText('0/1', (vs) => `Count: ${vs.count}`),
-            ]),
+            adoptElement('0', {}, []),
         );
     return [refManager.getPublicAPI(), render];
 }
