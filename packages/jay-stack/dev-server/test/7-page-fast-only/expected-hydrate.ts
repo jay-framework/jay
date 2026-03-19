82c3d3af87d47521d4152ee3a82fd94a37e8bbdd
yoav <yoavaa@gmail.com>
2026-03-19 18:48:06 +0200

    removed test 7b, renamed 7a to 7.

---
+++ packages/jay-stack/dev-server/test/7-page-fast-only/expected-hydrate.ts
@@ -0,0 +1,45 @@
+import {
+    ReferencesManager,
+    ConstructContext,
+    adoptText,
+    adoptElement,
+    childCompHydrate,
+    // @ts-ignore
+} from '/@fs{{ROOT}}/packages/runtime/runtime/dist/index.js';
+// @ts-ignore
+import { makeHeadlessInstanceComponent } from '/@fs{{ROOT}}/packages/jay-stack/stack-client-runtime/dist/index.js';
+// @ts-ignore
+import { widget } from '/widget.ts';
+function _headlessWidget0HydrateRender(options) {
+    const [refManager, [refIncrement]] = ReferencesManager.for(options, ['increment'], [], [], []);
+    const render = (viewState) =>
+        ConstructContext.withHydrationChildContext(viewState, refManager, () =>
+            adoptElement('0', {}, [
+                adoptText('0/0', (vs) => vs.label),
+                adoptText('0/1', (vs) => vs.value),
+                adoptElement('0/2', {}, [], refIncrement()),
+            ]),
+        );
+    return [refManager.getPublicAPI(), render];
+}
+const _HeadlessWidget0 = makeHeadlessInstanceComponent(
+    _headlessWidget0HydrateRender,
+    widget,
+    'widget:AR0',
+);
+export function hydrate(rootElement, options) {
+    const [refManager, [refAr0]] = ReferencesManager.for(options, [], [], ['ar0'], []);
+    const render = (viewState) =>
+        ConstructContext.withHydrationRootContext(viewState, refManager, rootElement, () =>
+            adoptElement('0', {}, [
+                adoptText('0/0', (vs) => vs.title),
+                childCompHydrate(
+                    _HeadlessWidget0,
+                    (vs) => ({ itemId: '1' }),
+                    '0/widget:AR0',
+                    refAr0(),
+                ),
+            ]),
+        );
+    return [refManager.getPublicAPI(), render];
+}
